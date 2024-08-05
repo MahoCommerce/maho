e9e64666fc15b921019a8570f81498f615ccb54b
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-08-05 17:30:53 +0100

    Added CLI command "serve"

---
+++ lib/MahoCLI/Commands/Serve.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Maho\Commands;
+
+use Mage;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'serve',
+    description: 'Run Maho with the built in server'
+)]
+class Serve extends BaseMahoCommand
+{
+    protected function configure(): void
+    {
+        $this->addArgument('port', InputArgument::OPTIONAL, 'Default is 8000', 8000);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $host = '127.0.0.1';
+        $port = $input->getArgument('port');
+        $docroot = MAHO_ROOT_DIR . '/pub';
+
+        $output->writeln("Serving Maho on http://{$host}:{$port}, press CTRL+C to exit...");
+        passthru("php -t {$host}:{$port} -t {$docroot}");
+        $output->writeln("Process interrupted.");
+
+        return Command::SUCCESS;
+    }
+}
