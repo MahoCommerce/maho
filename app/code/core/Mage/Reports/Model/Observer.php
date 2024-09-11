78f02c7aa7f4660fc031565eb423ebc4e3cc9220
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-09-11 01:13:13 +0100

    Added missing header

--- app/code/core/Mage/Reports/Model/Observer.php
+++ app/code/core/Mage/Reports/Model/Observer.php
@@ -1,6 +1,16 @@
 <?php
 /**
- * Reports Observer
+ * Maho
+ *
+ * @category   Mage
+ * @package    Mage_Reports
+ * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * @category   Mage
+ * @package    Mage_Reports
  */
 class Mage_Reports_Model_Observer
 {
