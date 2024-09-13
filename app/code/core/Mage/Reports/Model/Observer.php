68f4ffa7ec1b5f4e0fd9068ea8c61c64cbaad0e7
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-09-13 16:18:11 +0100

    Updated copyright

--- app/code/core/Mage/Reports/Model/Observer.php
+++ app/code/core/Mage/Reports/Model/Observer.php
@@ -6,6 +6,7 @@
  * @package    Mage_Reports
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
  */
 
 /**
