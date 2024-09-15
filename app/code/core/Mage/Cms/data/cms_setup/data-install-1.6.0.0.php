fbcea5fd57c7683d0857fc3ffc4699a279f2a16a
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-09-15 22:30:51 +0100

    Minor rebranding

--- app/code/core/Mage/Cms/data/cms_setup/data-install-1.6.0.0.php
+++ app/code/core/Mage/Cms/data/cms_setup/data-install-1.6.0.0.php
@@ -16,7 +16,7 @@ $cmsBlocks = [
         'identifier'    => 'footer_links',
         'content'       => '
 <ul>
-    <li><a href="{{store direct_url="about-magento-demo-store"}}">About Us</a></li>
+    <li><a href="{{store direct_url="about-maho-demo-store"}}">About Us</a></li>
     <li class="last"><a href="{{store direct_url="customer-service"}}">Customer Service</a></li>
 </ul>',
         'is_active'     => 1,
@@ -45,7 +45,7 @@ $cmsPages = [
 </dl>
 <dl>
     <dt>What can you do?</dt>
-    <dd>Have no fear, help is near! There are many ways you can get back on track with Magento Store.</dd>
+    <dd>Have no fear, help is near! There are many ways you can get back on track with Maho Store.</dd>
     <dd>
         <ul class="disc">
             <li><a href="#" onclick="history.go(-1); return false;">Go back</a> to the previous page.</li>
@@ -72,10 +72,10 @@ $cmsPages = [
     [
         'title'         => 'About Us',
         'root_template' => 'two_columns_right',
-        'identifier'    => 'about-magento-demo-store',
+        'identifier'    => 'about-maho-demo-store',
         'content'       => '
 <div class="page-title">
-    <h1>About Magento Store</h1>
+    <h1>About Maho Store</h1>
 </div>
 <div class="col3-set">
 <div class="col-1"><p style="line-height:1.2em;"><small>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
@@ -105,7 +105,7 @@ faucibus et, tempus at, dui. Nunc risus. Proin scelerisque augue. Nam ullamcorpe
 sit amet aliquet lectus dolor et quam. Pellentesque consequat luctus purus.</strong></p>
 <p>Nunc et risus. Etiam a nibh. Phasellus dignissim metus eget nisi.</p>
 <div class="divider"></div>
-<p>To all of you, from all of us at Magento Store - Thank you and Happy eCommerce!</p>
+<p>To all of you, from all of us at Maho Store - Thank you and Happy eCommerce!</p>
 <p style="line-height:1.2em;"><strong style="font:italic 2em Georgia, serif;">John Doe</strong><br />
 <small>Some important guy</small></p></div>
 </div>',
@@ -241,7 +241,7 @@ $content = '
         <strong><span>Company</span></strong>
     </div>
     <ul>
-        <li><a href="{{store url=""}}about-magento-demo-store/">About Us</a></li>
+        <li><a href="{{store url=""}}about-maho-demo-store/">About Us</a></li>
         <li><a href="{{store url=""}}contacts/">Contact Us</a></li>
         <li><a href="{{store url=""}}customer-service/">Customer Service</a></li>
         <li><a href="{{store url=""}}privacy-policy-cookie-restriction-mode/">Privacy Policy</a></li>
