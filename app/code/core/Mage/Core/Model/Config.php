a889669a7e76ee673c457c8467430c0da83d409c
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-09-17 22:44:44 +0100

    OM PR 4163

--- app/code/core/Mage/Core/Model/Config.php
+++ app/code/core/Mage/Core/Model/Config.php
@@ -1722,6 +1722,21 @@ class Mage_Core_Model_Config extends Mage_Core_Model_Config_Base
         return $this;
     }
 
+
+    /**
+     * Get config value from DB
+     *
+     * @param   string $path
+     * @param   string $scope
+     * @param   int $scopeId
+     * @return  string|false
+     */
+    public function getConfig(string $path, string $scope = 'default', int $scopeId = 0)
+    {
+        $resource = $this->getResourceModel();
+        return $resource->getConfig(rtrim($path, '/'), $scope, $scopeId);
+    }
+
     /**
      * Get fieldset from configuration
      *
