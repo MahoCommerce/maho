ea5f93731d7cfdc576529f24f79c2c06f32268e3
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-09-25 17:51:26 +0100

    OM PR 4226
    OpenMage/magento-lts#4226

---
+++ app/code/core/Mage/Widget/sql/widget_setup/mysql4-upgrade-1.6.0.0-1.6.0.1.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Maho
+ *
+ * @category   Mage
+ * @package    Mage_Widget
+ * @copyright  Copyright (c) 2024 The OpenMage Contributors (https://openmage.org)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/** @var Mage_Core_Model_Resource_Setup $installer */
+$installer = $this;
+$installer->startSetup();
+
+$installer->getConnection()->changeColumn(
+    $installer->getTable('widget/widget_instance_page'),
+    'page_group',
+    'page_group',
+    [
+        'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+        'length'    => 255
+    ]
+);
+
+$installer->getConnection()->changeColumn(
+    $installer->getTable('widget/widget_instance_page'),
+    'page_for',
+    'page_for',
+    [
+        'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+        'length'    => 255
+    ]
+);
