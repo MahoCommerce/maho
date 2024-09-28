86cf0c5721e967a587900b9ea3a988eab594f769
Justin Beaty <justin.beaty@gmail.com>
2024-09-28 09:20:59 -0700

    add install script

---
+++ app/code/core/Mage/Customer/sql/customer_setup/upgrade-1.6.2.0.7-1.6.2.0.8.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Maho
+ *
+ * @category   Mage
+ * @package    Mage_Customer
+ * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/** @var Mage_Customer_Model_Entity_Setup $installer */
+$installer = $this;
+$installer->startSetup();
+
+$installer->getConnection()
+    ->addColumn($installer->getTable('customer/customer_group'), 'customer_attribute_set_id', [
+        'type'     => Varien_Db_Ddl_Table::TYPE_SMALLINT,
+        'unsigned' => true,
+        'nullable' => false,
+        'default'  => 0,
+        'comment'  => 'Customer Group Attribute Set ID',
+    ]);
+
+$installer->getConnection()
+    ->addColumn($installer->getTable('customer/customer_group'), 'customer_address_attribute_set_id', [
+        'type'     => Varien_Db_Ddl_Table::TYPE_SMALLINT,
+        'unsigned' => true,
+        'nullable' => false,
+        'default'  => 0,
+        'comment'  => 'Customer Group Address Attribute Set ID',
+    ]);
+
+$installer->endSetup();
