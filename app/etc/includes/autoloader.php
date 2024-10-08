a7daf3f66984e36ec023336a68ff5f4894e34d24
Justin Beaty <justin.beaty@gmail.com>
2024-10-08 16:34:01 -0700

    remove test file

--- app/etc/includes/autoloader.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// spl_autoload_register(function ($class) {
-//     $class = ucwords($class, '_'); // Fix for classes with incorrect capitalization
-//     if ($file = stream_resolve_include_path(strtr($class, '_', DS) . '.php')) {
-//         return include $file;
-//     }
-// });
