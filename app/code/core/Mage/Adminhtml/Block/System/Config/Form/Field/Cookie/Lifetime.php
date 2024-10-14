9dd7de50912e997ade05dcee0ed7bf68b1dafa9a
Justin Beaty <justin.beaty@gmail.com>
2024-10-14 06:43:17 -0700

    translations

--- app/code/core/Mage/Adminhtml/Block/System/Config/Form/Field/Cookie/Lifetime.php
+++ app/code/core/Mage/Adminhtml/Block/System/Config/Form/Field/Cookie/Lifetime.php
@@ -30,7 +30,7 @@ class Mage_Adminhtml_Block_System_Config_Form_Field_Cookie_Lifetime extends Mage
             $max = Mage_Core_Controller_Front_Action::SESSION_MAX_LIFETIME;
         }
 
-        $element->setComment(Mage::helper('customer')->__('Value must be between %d and %d', $min, $max));
+        $element->setComment(Mage::helper('core')->__('Value must be between %d and %d', $min, $max));
         $element->addClass("validate-digits validate-digits-range digits-range-$min-$max");
 
         return parent::render($element);
