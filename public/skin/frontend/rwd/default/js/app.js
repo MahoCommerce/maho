c4ee30d088ad4d1c1919eb74b1e68282e2568305
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-10-20 15:22:16 +0100

    RWD: bugfix for skip-links open/close

--- public/skin/frontend/rwd/default/js/app.js
+++ public/skin/frontend/rwd/default/js/app.js
@@ -431,32 +431,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const skipContents = document.querySelectorAll('.skip-content');
     const skipLinks = document.querySelectorAll('.skip-link');
 
-    document.querySelector('.skip-links').addEventListener('click', function(e) {
-        if (e.target.classList.contains('skip-link')) {
-            e.preventDefault();
-            const self = e.target;
-            const target = self.getAttribute('data-target-element') || self.getAttribute('href');
-            const elem = document.querySelector(target);
-            const isSkipContentOpen = elem.classList.contains('skip-active');
+    document.querySelector('.skip-links').addEventListener('click', (e) => {
+        const skipLink = e.target.closest('.skip-link');
+        if (!skipLink) return;
 
-            // Hide all stubs
-            skipLinks.forEach(link => link.classList.remove('skip-active'));
-            skipContents.forEach(content => content.classList.remove('skip-active'));
+        e.preventDefault();
 
-            // Toggle stubs
-            if (isSkipContentOpen) {
-                self.classList.remove('skip-active');
-                elem.classList.remove('skip-active');
-            } else {
-                self.classList.add('skip-active');
-                elem.classList.add('skip-active');
-            }
+        const target = skipLink.getAttribute('data-target-element') || skipLink.getAttribute('href');
+        const elem = document.querySelector(target);
+        if (!elem) return;
 
-            if (target === '#header-search') {
-                const searchInput = document.getElementById('search');
-                if (searchInput) {
-                    searchInput.focus();
-                }
+        const isSkipContentOpen = elem.classList.contains('skip-active');
+        document.querySelectorAll('.skip-active').forEach(el => el.classList.remove('skip-active'));
+
+        if (!isSkipContentOpen) {
+            skipLink.classList.add('skip-active');
+            elem.classList.add('skip-active');
+        }
+
+        if (target === '#header-search') {
+            const searchInput = document.getElementById('search');
+            if (searchInput) {
+                searchInput.focus();
             }
         }
     });
@@ -465,10 +461,8 @@ document.addEventListener('DOMContentLoaded', () => {
         if (e.target.matches('#header-cart .skip-link-close')) {
             const parent = e.target.closest('.skip-content');
             const link = parent.parentElement.querySelector('.skip-link');
-
             parent.classList.remove('skip-active');
             link.classList.remove('skip-active');
-
             e.preventDefault();
         }
     });
