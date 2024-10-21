b39bfa0df7a87f0114d1f0c521e240ae62fc1b75
Justin Beaty <justin.beaty@gmail.com>
2024-10-21 08:50:52 -0700

    Fix mahoInfo calls

--- app/code/core/Mage/Core/Model/Maho/Api.php
+++ app/code/core/Mage/Core/Model/Maho/Api.php
@@ -5,7 +5,6 @@
  * @category   Mage
  * @package    Mage_Core
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
- * @copyright  Copyright (c) 2021-2023 The OpenMage Contributors (https://openmage.org)
  * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
@@ -15,8 +14,19 @@
  *
  * @category   Mage
  * @package    Mage_Core
- * @deprecated
  */
-class Mage_Core_Model_Magento_Api extends Mage_Core_Model_Maho_Api
+class Mage_Core_Model_Maho_Api extends Mage_Api_Model_Resource_Abstract
 {
+    /**
+     * Retrieve information about the current Maho installation
+     *
+     * @return array
+     */
+    public function info()
+    {
+        $result = [];
+        $result['maho_version'] = Mage::getMahoVersion();
+
+        return $result;
+    }
 }
