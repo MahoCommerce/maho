b2668f18a446455cba90527b3683190cff911998
Justin Beaty <justin.beaty@gmail.com>
2024-11-01 07:34:28 -0700

    Install script

--- app/code/core/Mage/Customer/sql/maho_setup/maho-24.10.0.php
+++ app/code/core/Mage/Customer/sql/maho_setup/maho-24.10.0.php
@@ -12,25 +12,59 @@
 $installer = $this;
 $installer->startSetup();
 
+/**
+ * Add new columns to the customer_group table allowing each group to be assigned
+ * an attribute set for both the customer and customer_address entity types.
+ *
+ * If there are no attribute sets available, create a new one and set as default.
+ */
 $defs = [
     [ 'model' => 'customer/customer', 'column' => 'customer_attribute_set_id' ],
     [ 'model' => 'customer/address', 'column' => 'customer_address_attribute_set_id' ],
 ];
 
 foreach ($defs as $info) {
-    $entityTypeId = Mage::getResourceModel($info['model'])
-                  ->getEntityType()
-                  ->getId();
+    /** @var Mage_Eav_Model_Entity_Type $entityType */
+    $entityType = Mage::getResourceModel($info['model'])->getEntityType();
 
-    $defaultSet = Mage::getResourceModel('eav/entity_attribute_set_collection')
-                ->setEntityTypeFilter($entityTypeId)
-                ->getFirstItem();
+    /** $var Mage_Eav_Model_Resource_Entity_Attribute_Set_Collection $collection */
+    $collection = Mage::getResourceModel('eav/entity_attribute_set_collection')
+                ->setEntityTypeFilter($entityType->getId());
 
-    if ($defaultSet->getAttributeSetId() === null) {
+    /** @var Mage_Eav_Model_Entity_Attribute_Set $defaultSet */
+    $defaultSet = $collection->getItemById($entityType->getDefaultAttributeSetId())
+                ?? $collection->getFirstItem();
+
+    if ($defaultSet->getId() === null) {
+        /** @var Mage_Eav_Model_Entity_Attribute_Set $defaultSet */
         $defaultSet = Mage::getModel('eav/entity_attribute_set')
-                    ->setEntityTypeId($entityTypeId)
+                    ->setEntityTypeId($entityType->getId())
                     ->setAttributeSetName('Default')
                     ->save();
+
+        /** @var Mage_Eav_Model_Entity_Attribute_Group $modelGroup */
+        $modelGroup = Mage::getModel('eav/entity_attribute_group')
+                    ->setAttributeGroupName('General')
+                    ->setAttributeSetId($defaultSet->getId())
+                    ->setSortOrder(1)
+                    ->setDefaultId(1)
+                    ->save();
+
+        /** @var Mage_Eav_Model_Resource_Entity_Attribute_Collection $attributes */
+        $attributes = Mage::getResourceModel($entityType->getEntityAttributeCollection())
+                    ->setEntityTypeFilter($entityType->getId())
+                    ->load();
+
+        /** @var Mage_Eav_Model_Entity_Attribute $attribute */
+        foreach ($attributes->getItems() as $attribute) {
+            $attribute->setAttributeGroupId($modelGroup->getId())
+                      ->setAttributeSetId($defaultSet->getId())
+                      ->save();
+        }
+    }
+
+    if ($entityType->getDefaultAttributeSetId() !== $defaultSet->getId()) {
+        $entityType->setDefaultAttributeSetId($defaultSet->getId())->save();
     }
 
     $installer->getConnection()
@@ -38,7 +72,7 @@ foreach ($defs as $info) {
                   'type'     => Varien_Db_Ddl_Table::TYPE_SMALLINT,
                   'unsigned' => true,
                   'nullable' => false,
-                  'default'  => $defaultSet->getAttributeSetId(),
+                  'default'  => $defaultSet->getId(),
                   'comment'  => 'Customer Group Attribute Set ID',
               ]);
 }
