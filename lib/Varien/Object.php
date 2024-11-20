6212df9f09377a7f7c7296e88bce921b598f75c8
Justin Beaty <51970393+justinbeaty@users.noreply.github.com>
2024-11-20 08:38:05 -0800

    Added JsonSerializable interface to Varien_Object (#55)

--- lib/Varien/Object.php
+++ lib/Varien/Object.php
@@ -16,7 +16,7 @@
  * @category   Varien
  * @package    Varien_Object
  */
-class Varien_Object implements ArrayAccess
+class Varien_Object implements ArrayAccess, JsonSerializable
 {
     /**
      * Object attributes
@@ -560,6 +560,17 @@ class Varien_Object implements ArrayAccess
     public function toJson(array $arrAttributes = [])
     {
         return $this->__toJson($arrAttributes);
+    }
+
+    /**
+     * Implementation of JsonSerializable::jsonSerialize()
+     *
+     * @link https://www.php.net/manual/en/jsonserializable.jsonserialize.php
+     */
+    #[\Override]
+    public function jsonSerialize(): array
+    {
+        return $this->__toArray();
     }
 
     /**
