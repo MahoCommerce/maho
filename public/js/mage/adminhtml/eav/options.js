5d3fcf22c19aabb27be0ff3ad9d4eb95a3734ad9
Justin Beaty <justin.beaty@gmail.com>
2024-11-24 06:58:42 -0800

    copyright

--- public/js/mage/adminhtml/eav/options.js
+++ public/js/mage/adminhtml/eav/options.js
@@ -1,4 +1,13 @@
 /**
+ * Maho
+ *
+ * @category    Mage
+ * @package     Mage_Adminhtml
+ * @copyright   Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @license     https://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+
+/**
  *
  */
 class EavAttributeOptionsForm {
