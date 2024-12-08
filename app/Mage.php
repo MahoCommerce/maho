bfb4649817d6517b34999a597615ef13776e94e0
Justin Beaty <justin.beaty@gmail.com>
2024-12-08 07:48:03 -0800

    Allow passing `'is_installed' => false` to Mage::init to disable loading local.xml

--- app/Mage.php
+++ app/Mage.php
@@ -663,8 +663,8 @@ final class Mage
      */
     protected static function _setIsInstalled($options = [])
     {
-        if (isset($options['is_installed']) && $options['is_installed']) {
-            self::$_isInstalled = true;
+        if (isset($options['is_installed'])) {
+            self::$_isInstalled = (bool) $options['is_installed'];
         }
     }
 
