e054b17902eb5814abdc35f4bb53b87fa5e0363e
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-12-12 21:13:49 +0000

    Removed prototypejs from varien/product.js, varien/configurable.js and configurableswatches/swatches-product.js (#68)
    Co-authored-by: Justin Beaty <51970393+justinbeaty@users.noreply.github.com>

--- public/js/prototype/prototype.js
+++ public/js/prototype/prototype.js
@@ -815,42 +815,6 @@ Object.extend(String.prototype, (function() {
   };
 })());
 
-var Template = Class.create({
-  initialize: function(template, pattern) {
-    this.template = template.toString();
-    this.pattern = pattern || Template.Pattern;
-  },
-
-  evaluate: function(object) {
-    if (object && Object.isFunction(object.toTemplateReplacements))
-      object = object.toTemplateReplacements();
-
-    return this.template.gsub(this.pattern, function(match) {
-      if (object == null) return (match[1] + '');
-
-      var before = match[1] || '';
-      if (before == '\\') return match[2];
-
-      var ctx = object, expr = match[3],
-          pattern = /^([^.[]+|\[((?:.*?[^\\])?)\])(\.|\[|$)/;
-
-      match = pattern.exec(expr);
-      if (match == null) return before;
-
-      while (match != null) {
-        var comp = match[1].startsWith('[') ? match[2].replace(/\\\\]/g, ']') : match[1];
-        ctx = ctx[comp];
-        if (null == ctx || '' == match[3]) break;
-        expr = expr.substring('[' == match[3] ? match[1].length : match[0].length);
-        match = pattern.exec(expr);
-      }
-
-      return before + String.interpret(ctx);
-    });
-  }
-});
-Template.Pattern = /(^|.|\r|\n)(#\{(.*?)\})/;
-
 var $break = { };
 
 var Enumerable = (function() {
