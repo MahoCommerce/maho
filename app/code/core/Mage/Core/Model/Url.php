7555d68af59d7accfc30230a7e0b040e433e21cf
Justin Beaty <justin.beaty@gmail.com>
2024-12-29 10:11:04 -0800

    use unset

--- app/code/core/Mage/Core/Model/Url.php
+++ app/code/core/Mage/Core/Model/Url.php
@@ -869,7 +869,7 @@ class Mage_Core_Model_Url extends Varien_Object
     public function purgeQueryParams()
     {
         $this->unsetData('query');
-        $this->setData('query_params', []);
+        $this->unsetData('query_params');
         return $this;
     }
 
