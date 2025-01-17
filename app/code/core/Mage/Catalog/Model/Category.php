fc2a352f263c58451ac6e257b4b2ff90e7bbcbb3
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-01-17 13:41:11 +0000

    Frontend: Added width/height HTML attributes to category main image

--- app/code/core/Mage/Catalog/Model/Category.php
+++ app/code/core/Mage/Catalog/Model/Category.php
@@ -528,11 +528,21 @@ class Mage_Catalog_Model_Category extends Mage_Catalog_Model_Abstract
      */
     public function getImageUrl()
     {
-        $url = false;
         if ($image = $this->getImage()) {
-            $url = Mage::getBaseUrl('media') . 'catalog/category/' . $image;
+            return Mage::getBaseUrl('media') . 'catalog/category/' . $image;
         }
-        return $url;
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getImagePath()
+    {
+        if ($image = $this->getImage()) {
+            return Mage::getBaseDir('media') . '/catalog/category/' . $image;
+        }
+        return '';
     }
 
     /**
