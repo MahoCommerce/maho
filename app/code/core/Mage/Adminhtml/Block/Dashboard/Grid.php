a52cd76549926cd3212358db569e2a93ee4e87b5
Justin Beaty <justin.beaty@gmail.com>
2025-02-01 08:17:19 -0800

    Dashboard

--- app/code/core/Mage/Adminhtml/Block/Dashboard/Grid.php
+++ app/code/core/Mage/Adminhtml/Block/Dashboard/Grid.php
@@ -6,6 +6,7 @@
  * @package    Mage_Adminhtml
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2022-2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -23,7 +24,7 @@ class Mage_Adminhtml_Block_Dashboard_Grid extends Mage_Adminhtml_Block_Widget_Gr
     public function __construct()
     {
         parent::__construct();
-        $this->setTemplate('dashboard/grid.phtml');
+        //$this->setTemplate('dashboard/grid.phtml');
         $this->setDefaultLimit(5);
     }
 }
