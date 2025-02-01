508bee287f1b03fe96c31d28e209f47e6676974e
Justin Beaty <justin.beaty@gmail.com>
2025-02-01 09:42:58 -0800

    Product Config: AJAX Result Rendering (#106)

--- app/code/core/Mage/Adminhtml/Helper/Catalog/Product/Composite.php
+++ app/code/core/Mage/Adminhtml/Helper/Catalog/Product/Composite.php
@@ -6,7 +6,7 @@
  * @package    Mage_Adminhtml
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2019-2023 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2024-2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -20,20 +20,6 @@ class Mage_Adminhtml_Helper_Catalog_Product_Composite extends Mage_Core_Helper_A
     protected $_moduleName = 'Mage_Adminhtml';
 
     /**
-     * Init layout of product configuration update result
-     *
-     * @param Mage_Adminhtml_Controller_Action $controller
-     * @return $this
-     */
-    protected function _initUpdateResultLayout($controller)
-    {
-        $controller->getLayout()->getUpdate()
-            ->addHandle('ADMINHTML_CATALOG_PRODUCT_COMPOSITE_UPDATE_RESULT');
-        $controller->loadLayoutUpdates()->generateLayoutXml()->generateLayoutBlocks();
-        return $this;
-    }
-
-    /**
      * Prepares and render result of composite product configuration update for a case
      * when single configuration submitted
      *
@@ -42,34 +28,7 @@ class Mage_Adminhtml_Helper_Catalog_Product_Composite extends Mage_Core_Helper_A
      */
     public function renderUpdateResult($controller, Varien_Object $updateResult)
     {
-        Mage::register('composite_update_result', $updateResult);
-
-        $this->_initUpdateResultLayout($controller);
-        $controller->renderLayout();
-        return $this;
-    }
-
-    /**
-    * Init composite product configuration layout
-    *
-    * $isOk - true or false, whether action was completed nicely or with some error
-    * If $isOk is FALSE (some error during configuration), so $productType must be null
-    *
-    * @param Mage_Adminhtml_Controller_Action $controller
-    * @param bool $isOk
-    * @param string $productType
-    * @return $this
-    */
-    protected function _initConfigureResultLayout($controller, $isOk, $productType)
-    {
-        $update = $controller->getLayout()->getUpdate();
-        if ($isOk) {
-            $update->addHandle('ADMINHTML_CATALOG_PRODUCT_COMPOSITE_CONFIGURE')
-                ->addHandle('PRODUCT_TYPE_' . $productType);
-        } else {
-            $update->addHandle('ADMINHTML_CATALOG_PRODUCT_COMPOSITE_CONFIGURE_ERROR');
-        }
-        $controller->loadLayoutUpdates()->generateLayoutXml()->generateLayoutBlocks();
+        $controller->getResponse()->setBodyJson($updateResult);
         return $this;
     }
 
@@ -98,41 +57,45 @@ class Mage_Adminhtml_Helper_Catalog_Product_Composite extends Mage_Core_Helper_A
             $product = Mage::getModel('catalog/product')
                 ->setStoreId($currentStoreId)
                 ->load($configureResult->getProductId());
+
             if (!$product->getId()) {
                 Mage::throwException($this->__('Product is not loaded.'));
             }
+
             Mage::register('current_product', $product);
             Mage::register('product', $product);
 
             // Register customer we're working with
-            $currentCustomer = $configureResult->getCurrentCustomer();
-            if (!$currentCustomer) {
-                $currentCustomerId = (int) $configureResult->getCurrentCustomerId();
-                if ($currentCustomerId) {
-                    $currentCustomer = Mage::getModel('customer/customer')
-                        ->load($currentCustomerId);
-                }
+
+            if (!$configureResult->getCurrentCustomer() && $configureResult->getCurrentCustomerId()) {
+                $configureResult->setCurrentCustomer(
+                    Mage::getModel('customer/customer')->load((int) $configureResult->getCurrentCustomerId()),
+                );
             }
-            if ($currentCustomer) {
-                Mage::register('current_customer', $currentCustomer);
+            if ($configureResult->getCurrentCustomer()) {
+                Mage::register('current_customer', $configureResult->getCurrentCustomer());
             }
 
             // Prepare buy request values
-            $buyRequest = $configureResult->getBuyRequest();
-            if ($buyRequest) {
+            if ($buyRequest = $configureResult->getBuyRequest()) {
                 Mage::helper('catalog/product')->prepareProductOptions($product, $buyRequest);
             }
 
-            $isOk = true;
-            $productType = $product->getTypeId();
+            $controller->getLayout()->getUpdate()
+                ->addHandle('ADMINHTML_CATALOG_PRODUCT_COMPOSITE_CONFIGURE')
+                ->addHandle('PRODUCT_TYPE_' . $product->getTypeId());
+
+            $controller->loadLayoutUpdates()
+                ->generateLayoutXml()
+                ->generateLayoutBlocks()
+                ->renderLayout();
+
+        } catch (Mage_Core_Exception $e) {
+            $controller->getResponse()->setBodyJson([ 'error' => true, 'message' => $e->getMessage() ]);
         } catch (Exception $e) {
-            $isOk = false;
-            $productType = null;
-            Mage::register('composite_configure_result_error_message', $e->getMessage());
+            $controller->getResponse()->setBodyJson([ 'error' => true, 'message' => $this->__('Internal Error') ]);
         }
 
-        $this->_initConfigureResultLayout($controller, $isOk, $productType);
-        $controller->renderLayout();
         return $this;
     }
 }
