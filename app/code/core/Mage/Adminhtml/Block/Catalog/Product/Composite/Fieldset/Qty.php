b49f4552bcd721821dd2fdac4fcb6cec99214d03
Justin Beaty <justin.beaty@gmail.com>
2025-02-02 07:17:13 -0800

    Product Config: Adminhtml Templates + JS Dialog Controller

--- app/code/core/Mage/Adminhtml/Block/Catalog/Product/Composite/Fieldset/Qty.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Product/Composite/Fieldset/Qty.php
@@ -6,7 +6,7 @@
  * @package    Mage_Adminhtml
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2022-2024 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2024-2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -44,10 +44,7 @@ class Mage_Adminhtml_Block_Catalog_Product_Composite_Fieldset_Qty extends Mage_C
      */
     public function getQtyValue()
     {
-        $qty = $this->getProduct()->getPreconfiguredValues()->getQty();
-        if (!$qty) {
-            $qty = 1;
-        }
-        return $qty;
+        $qty = (int) $this->getProduct()->getPreconfiguredValues()->getQty();
+        return $qty ? $qty : 1;
     }
 }
