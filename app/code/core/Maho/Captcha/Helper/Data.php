6b81dd7526f7575a96e91b80695143997727ee6f
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-02-17 13:04:54 +0000

    work

--- app/code/core/Maho/Captcha/Helper/Data.php
+++ app/code/core/Maho/Captcha/Helper/Data.php
@@ -2,23 +2,43 @@
 
 class Maho_Captcha_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    public const XML_PATH_ENABLED = 'maho_captcha/settings/enabled';
-    public const XML_PATH_HMAC_KEY = 'maho_captcha/settings/hmac_key';
+    public const XML_PATH_ENABLED = 'admin/maho_captcha/enabled';
+    public const XML_PATH_HMAC_KEY = 'admin/maho_captcha/hmac_key';
+    public const XML_PATH_FRONTEND_SELECTORS = "admin/maho_captcha/selectors";
 
     public function isEnabled(): bool
     {
-        return Mage::getStoreConfig(self::XML_PATH_ENABLED);
+        return true;
+        return $this->isModuleEnabled() && $this->isModuleOutputEnabled() && Mage::getStoreConfigFlag(self::XML_PATH_ENABLED);
     }
 
     public function getHmacKey(): string
     {
-        return Mage::getStoreConfig(self::XML_PATH_HMAC_KEY);
+        return Mage::getStoreConfig(self::XML_PATH_HMAC_KEY) ?? '';
+    }
+
+    public function getFrontendSelectors(): string
+    {
+        $selectors = Mage::getStoreConfig(self::XML_PATH_FRONTEND_SELECTORS) ?? '';
+        $selectors = trim($selectors);
+        $selectors = str_replace(["\r\n", "\r"], "\n", $selectors);
+        $selectors = explode("\n", $selectors);
+
+        $selectorsToKeep = [];
+        foreach ($selectors as $selector) {
+            $selector = trim($selector);
+            if (strlen($selector) && !str_starts_with($selector, '//')) {
+                $selectorsToKeep[] = $selector;
+            }
+        }
+
+        return implode(',', $selectorsToKeep);
     }
 
     public function verify(string $payload): bool
     {
         try {
-            $hmacKey = Mage::helper('mahocaptcha')->getHmacKey();
+            $hmacKey = (string) Mage::getConfig()->getNode('global/crypt/key');
             $altcha = new \Altcha\Altcha($hmacKey);
             return $altcha->verifySolution($payload);
         } catch (Exception $e) {
