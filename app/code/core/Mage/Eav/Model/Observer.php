a25ddedd6716ba303d4d9d8dfc3b3875f47349e6
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-02-19 11:33:40 +0000

    EAV cleaner

--- app/code/core/Mage/Eav/Model/Observer.php
+++ app/code/core/Mage/Eav/Model/Observer.php
@@ -5,6 +5,7 @@
  *
  * @package    Mage_Eav
  * @copyright  Copyright (c) 2019-2022 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
