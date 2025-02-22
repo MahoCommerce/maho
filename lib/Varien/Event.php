fbc40d4979a2d9d82306b0d1bcbb783466fb839f
Justin Beaty <justin.beaty@gmail.com>
2025-02-22 06:47:53 -0800

    fixes

--- lib/Varien/Event.php
+++ lib/Varien/Event.php
@@ -6,7 +6,7 @@
  * @package    Varien_Event
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2022-2023 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2024-2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -50,7 +50,7 @@ class Varien_Event extends Varien_Object
      * ```php
      * public function process(Varien_Event_Observer $observer): void
      * {
-     *     $isAjax = (bool) $observer->getEvent()->args['is_ajax'];
+     *     $isAjax = (bool) ($observer->getEvent()->args['is_ajax'] ?? null);
      *     // ...
      * }
      * ```
