11433a65fa77a516e38c52a89260f6b6bdd6af9e
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-02-24 20:37:56 +0000

    Revert "removed cleaner"
    This reverts commit 51b825aad97750e586083e26bac39ae6903f1f59.

--- app/code/core/Mage/Eav/Model/Observer.php
+++ app/code/core/Mage/Eav/Model/Observer.php
@@ -31,4 +31,55 @@ class Mage_Eav_Model_Observer
             Mage::getSingleton('eav/config')->setCurrentStoreId(Mage::app()->getStore()->getId());
         }
     }
+
+    public function cleanOrphanedRecords()
+    {
+        $resource = Mage::getSingleton('core/resource');
+        $connection = $resource->getConnection('core_write');
+        $entityAttributeTable = $resource->getTableName('eav_entity_attribute');
+        $attributeSetTable = $resource->getTableName('eav_attribute_set');
+        $entityTypes = Mage::getModel('eav/entity_type')->getCollection();
+        foreach ($entityTypes as $entityType) {
+            $entityTypeId = $entityType->getEntityTypeId();
+
+            try {
+                $entityTable = $resource->getTableName($entityType->getEntityTable());
+            } catch (Mage_Core_Exception $e) {
+                // If the entityTable doesn't exist, it could be an entity created by a module
+                // that was later removed, simply skip it at the moment
+                continue;
+            }
+
+            $attributeSets = $connection->fetchCol("SELECT attribute_set_id FROM $attributeSetTable WHERE entity_type_id=$entityTypeId");
+            $attributeTables = [
+                "{$entityTable}_char",
+                "{$entityTable}_datetime",
+                "{$entityTable}_decimal",
+                "{$entityTable}_int",
+                "{$entityTable}_text",
+                "{$entityTable}_varchar",
+            ];
+            foreach ($attributeTables as $table) {
+                if (!$connection->isTableExists($table)) {
+                    continue;
+                }
+
+                foreach ($attributeSets as $attributeSetId) {
+                    try {
+                        $connection->query("
+                            DELETE FROM $table WHERE entity_type_id=$entityTypeId
+                            AND entity_id IN (
+                                SELECT entity_id from $entityTable WHERE entity_type_id=$entityTypeId AND attribute_set_id=$attributeSetId
+                            )
+                            AND attribute_id NOT IN (
+                                SELECT attribute_id FROM eav_entity_attribute WHERE entity_type_id=$entityTypeId AND attribute_set_id=$attributeSetId
+                            )
+                        ");
+                    } catch (Exception $e) {
+                        Mage::logException($e);
+                    }
+                }
+            }
+        }
+    }
 }
