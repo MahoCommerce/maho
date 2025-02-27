bdd49ffeebc7cc1ffba1e64541c2ace8d039d45b
Justin Beaty <justin.beaty@gmail.com>
2025-02-27 06:28:46 -0800

    Product Config: Frontend Templates (#106)

--- app/code/core/Mage/Catalog/Block/Product/View/Type/Configurable.php
+++ app/code/core/Mage/Catalog/Block/Product/View/Type/Configurable.php
@@ -6,6 +6,7 @@
  * @package    Mage_Catalog
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2020-2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -346,5 +347,23 @@ class Mage_Catalog_Block_Product_View_Type_Configurable extends Mage_Catalog_Blo
     protected function _convertPrice($price, $round = false)
     {
         return $this->_getHelper()->convertPrice($price, $round);
+    }
+
+    /**
+     * Render attribute using item renderer block
+     */
+    protected function renderAttributeBlock(Mage_Catalog_Model_Product_Type_Configurable_Attribute $attribute): ?string
+    {
+        $product = $this->getProduct();
+        $jsonConfig = $this->getJsonConfig();
+        foreach ($this->getChild('attr_renderers')->getSortedChildren() as $rendererName) {
+            $renderer = $this->getChild('attr_renderers')->getChild($rendererName);
+            if (method_exists($renderer, 'shouldRender') && $renderer->shouldRender($attribute, $jsonConfig)) {
+                $renderer->setProduct($product);
+                $renderer->setAttributeObj($attribute);
+                return $renderer->toHtml();
+            }
+        }
+        return null;
     }
 }
