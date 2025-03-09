eb30d9ec049b90d9f8afc0dfa4d2689f695cdb6f
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-03-09 12:14:30 +0100

    Renamed mahorecaptcha to mahocaptcha

--- public/js/maho-captcha.js
+++ public/js/maho-captcha.js
@@ -6,7 +6,7 @@
  * @license     https://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 
-const MahoRecaptcha = {
+const MahoCaptcha = {
     altchaWidget: null,
     altchaState: null,
     frontendSelectors: '',
