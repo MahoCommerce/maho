2d2ae08dbfdcd580a6e173316333ff417c61a42e
Justin Beaty <justin.beaty@gmail.com>
2025-03-12 16:20:13 -0700

    PHPStan Baseline: lib/Varien
    https://github.com/OpenMage/magento-lts/pull/4405

--- lib/Varien/Directory/IFactory.php
+++ lib/Varien/Directory/IFactory.php
@@ -6,16 +6,16 @@
  * @package    Varien_Directory
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2022-2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
  * Factory Interface for Directory
- * *
+ *
  * @package    Varien_Directory
  */
-
-interface IFactory
+interface Varien_Directory_IFactory
 {
     public function getFilesName(&$files);
     public function getFilesPaths(&$paths);
