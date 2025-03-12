3c2f93588cdfb61f9aff379fd164c86c518b420d
Justin Beaty <justin.beaty@gmail.com>
2025-03-12 09:35:51 -0700

    PHPStan Baseline: Mage_Dataflow

--- app/code/core/Mage/Dataflow/Model/Convert/Iterator/Http.php
+++ app/code/core/Mage/Dataflow/Model/Convert/Iterator/Http.php
@@ -6,7 +6,7 @@
  * @package    Mage_Dataflow
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2022-2023 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2024-2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -39,6 +39,12 @@ class Mage_Dataflow_Model_Convert_Iterator_Http extends Mage_Dataflow_Model_Conv
                 fclose($fp);
             }
         }
+        return $this;
+    }
+
+    #[\Override]
+    public function save()
+    {
         return $this;
     }
 }
