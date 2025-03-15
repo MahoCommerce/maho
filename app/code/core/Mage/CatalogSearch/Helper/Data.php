ea9ceaf826c3133023204bd26e7e500ea39d7af9
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-03-15 18:50:28 +0000

    copyright

--- app/code/core/Mage/CatalogSearch/Helper/Data.php
+++ app/code/core/Mage/CatalogSearch/Helper/Data.php
@@ -6,7 +6,7 @@
  * @package    Mage_CatalogSearch
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2019-2023 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2024-2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -348,10 +348,5 @@ class Mage_CatalogSearch_Helper_Data extends Mage_Core_Helper_Abstract
         }
 
         return $this->_engine;
-    }
-
-    public function isAdvancedSearchEnabled(): bool
-    {
-        return Mage::getStoreConfigFlag('catalog/advanced_search/enabled');
     }
 }
