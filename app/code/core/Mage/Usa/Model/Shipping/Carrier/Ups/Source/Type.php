5df99f83a4ab3487b42a68056b825a6dac567e49
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-03-20 11:38:33 +0100

    copyright

--- app/code/core/Mage/Usa/Model/Shipping/Carrier/Ups/Source/Type.php
+++ app/code/core/Mage/Usa/Model/Shipping/Carrier/Ups/Source/Type.php
@@ -6,14 +6,10 @@
  * @package    Mage_Usa
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2022-2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-/**
- * Usa Ups type action Dropdown source
- *
- * @package    Mage_Usa
- */
 class Mage_Usa_Model_Shipping_Carrier_Ups_Source_Type
 {
     public function toOptionArray()
