729ac829a9510ca1929e7c13f2ad2316189bf061
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-03-22 11:02:46 +0100

    update

--- lib/MahoCLI/Commands/PhpstormMetadataGenerate.php
+++ lib/MahoCLI/Commands/PhpstormMetadataGenerate.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Maho
+ *
+ * @package    MahoCLI
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
 namespace MahoCLI\Commands;
 
 use Mage;
