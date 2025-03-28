a20f923436307268d82c680cca3d009765dcae63
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-03-28 10:38:39 +0000

    copy

--- app/code/core/Mage/Catalog/Model/Product/Image.php
+++ app/code/core/Mage/Catalog/Model/Product/Image.php
@@ -6,7 +6,7 @@
  * @package    Mage_Catalog
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2017-2024 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2024-2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -14,7 +14,6 @@
  * Catalog product link model
  *
  * @package    Mage_Catalog
- *
  * @method $this setImageOpacity(int $value)
  */
 class Mage_Catalog_Model_Product_Image extends Mage_Core_Model_Abstract
