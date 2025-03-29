99bd04f5d904bf0f1291d4be60bc21307b1b4bd3
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-03-29 23:07:59 +0000

    work

--- app/code/core/Mage/Install/Model/Installer.php
+++ app/code/core/Mage/Install/Model/Installer.php
@@ -6,15 +6,10 @@
  * @package    Mage_Install
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2019-2025 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2024-2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-/**
- * Installer model
- *
- * @package    Mage_Install
- */
 class Mage_Install_Model_Installer extends Varien_Object
 {
     /**
