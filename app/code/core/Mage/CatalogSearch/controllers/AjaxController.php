e1080efab2aef0d3d473c3c8b50de377b23c14a6
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-03-31 22:12:09 +0100

    work

--- app/code/core/Mage/CatalogSearch/controllers/AjaxController.php
+++ app/code/core/Mage/CatalogSearch/controllers/AjaxController.php
@@ -6,15 +6,10 @@
  * @package    Mage_CatalogSearch
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2022-2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-/**
- * Catalog Search Controller
- *
- * @package    Mage_CatalogSearch
- * @module     Catalog
- */
 class Mage_CatalogSearch_AjaxController extends Mage_Core_Controller_Front_Action
 {
     public function suggestAction(): void
