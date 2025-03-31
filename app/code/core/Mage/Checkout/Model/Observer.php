287024bd480c725523b006201076ef73c2d79a41
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-03-31 18:54:46 +0100

    Migrated encryption library from `mcrypt` to `libsodium` (#140)
    Co-authored-by: Justin Beaty <51970393+justinbeaty@users.noreply.github.com>

--- app/code/core/Mage/Checkout/Model/Observer.php
+++ app/code/core/Mage/Checkout/Model/Observer.php
@@ -6,15 +6,10 @@
  * @package    Mage_Checkout
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2020-2023 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2024-2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-/**
- * Checkout observer model
- *
- * @package    Mage_Checkout
- */
 class Mage_Checkout_Model_Observer
 {
     public function unsetAll()
@@ -43,5 +38,33 @@ class Mage_Checkout_Model_Observer
         if ($quote->getIsCheckoutCart()) {
             Mage::getSingleton('checkout/session')->getQuoteId($quote->getId());
         }
+    }
+
+    public function encryptionKeyRegenerated(Varien_Event_Observer $observer): void
+    {
+        /** @var \Symfony\Component\Console\Output\OutputInterface $output */
+        $output = $observer->getEvent()->getOutput();
+        $encryptCallback = $observer->getEvent()->getEncryptCallback();
+        $decryptCallback = $observer->getEvent()->getDecryptCallback();
+        $readConnection = Mage::getSingleton('core/resource')->getConnection('core_read');
+        $writeConnection = Mage::getSingleton('core/resource')->getConnection('core_write');
+
+        $output->write('Re-encrypting data on sales_flat_quote table... ');
+
+        $table = Mage::getSingleton('core/resource')->getTableName('sales_flat_quote');
+        $select = $readConnection->select()
+            ->from($table)
+            ->where('password_hash IS NOT NULL');
+        $encryptedData = $readConnection->fetchAll($select);
+
+        foreach ($encryptedData as $encryptedDataRow) {
+            $writeConnection->update(
+                $table,
+                ['password_hash' => $encryptCallback($decryptCallback($encryptedDataRow['password_hash']))],
+                ['entity_id = ?' => $encryptedDataRow['entity_id']],
+            );
+        }
+
+        $output->writeln('OK');
     }
 }
