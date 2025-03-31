4c9149a8b05ce7223d4c5531d8dd1f426a7484d3
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-03-31 14:08:38 +0100

    work

--- app/code/core/Mage/CatalogSearch/controllers/AjaxController.php
+++ app/code/core/Mage/CatalogSearch/controllers/AjaxController.php
@@ -25,7 +25,7 @@ class Mage_CatalogSearch_AjaxController extends Mage_Core_Controller_Front_Actio
 
         $this->getResponse()->setBody(
             $this->getLayout()->createBlock('catalogsearch/autocomplete')
-                ->setTemplate('catalog/product/list.phtml')
+                ->setTemplate('catalogsearch/suggest.phtml')
                 ->toHtml()
         );
     }
