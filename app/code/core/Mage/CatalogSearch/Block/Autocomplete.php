d3e1498fb0bffd84ee273c0686bf4d4b01f10036
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-03-31 21:34:25 +0100

    work

--- app/code/core/Mage/CatalogSearch/Block/Autocomplete.php
+++ app/code/core/Mage/CatalogSearch/Block/Autocomplete.php
@@ -10,31 +10,6 @@
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-/**
- * Autocomplete queries list
- *
- * @package    Mage_CatalogSearch
- */
 class Mage_CatalogSearch_Block_Autocomplete extends Mage_Core_Block_Template
 {
-    protected ?Mage_CatalogSearch_Model_Resource_Fulltext_Collection $productCollection = null;
-
-    public function getLoadedProductCollection(): Mage_CatalogSearch_Model_Resource_Fulltext_Collection
-    {
-        if (!$this->productCollection) {
-            /** @var Mage_CatalogSearch_Helper_Data $helper */
-            $helper = $this->helper('catalogsearch');
-            $query = $helper->getQueryText();
-
-            $productCollection = Mage::getResourceModel('catalogsearch/fulltext_collection');
-            $productCollection->addSearchFilter($query)
-                ->setOrder('relevance', 'desc')
-                ->setPageSize(10);
-            Mage::getModel('catalog/layer')->prepareProductCollection($productCollection);
-
-            $this->productCollection = $productCollection;
-        }
-
-        return $this->productCollection;
-    }
 }
--- app/code/core/Mage/CatalogSearch/Block/Autocomplete/Product/List.php
+++ app/code/core/Mage/CatalogSearch/Block/Autocomplete/Product/List.php
@@ -12,8 +12,22 @@ class Mage_CatalogSearch_Block_Autocomplete_Product_List extends Mage_Catalog_Bl
         return '';
     }
 
-    public function getLoadedProductCollection()
+    public function getLoadedProductCollection(): Mage_CatalogSearch_Model_Resource_Fulltext_Collection
     {
-        return $this->_getProductCollection();
+        if (!$this->_productCollection) {
+            /** @var Mage_CatalogSearch_Helper_Data $helper */
+            $helper = $this->helper('catalogsearch');
+            $query = $helper->getQueryText();
+
+            $productCollection = Mage::getResourceModel('catalogsearch/fulltext_collection');
+            $productCollection->addSearchFilter($query)
+                ->setOrder('relevance', 'desc')
+                ->setPageSize(10);
+            Mage::getModel('catalog/layer')->prepareProductCollection($productCollection);
+
+            $this->_productCollection = $productCollection;
+        }
+
+        return $this->_productCollection;
     }
 }
