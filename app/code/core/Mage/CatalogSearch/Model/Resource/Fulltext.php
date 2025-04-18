ee98fa4a4c74340d9767ed0f471cde4a8d4048a6
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-04-18 18:27:06 +0200

    Fixed copyright headers

--- app/code/core/Mage/CatalogSearch/Model/Resource/Fulltext.php
+++ app/code/core/Mage/CatalogSearch/Model/Resource/Fulltext.php
@@ -6,15 +6,10 @@
  * @package    Mage_CatalogSearch
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2018-2025 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2024-2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-/**
- * CatalogSearch Fulltext Index resource model
- *
- * @package    Mage_CatalogSearch
- */
 class Mage_CatalogSearch_Model_Resource_Fulltext extends Mage_Core_Model_Resource_Db_Abstract
 {
     /**
