b6c365abff97234aff8c15795a52a7c6553451f7
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-04-29 13:47:50 +0200

    copyright

--- app/code/core/Mage/Core/Model/Magento/Api.php
+++ app/code/core/Mage/Core/Model/Magento/Api.php
@@ -6,14 +6,12 @@
  * @package    Mage_Core
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2021-2024 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2024-2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
  * Maho info API
- *
- * @package    Mage_Core
  * @deprecated
  */
 class Mage_Core_Model_Magento_Api extends Mage_Core_Model_Maho_Api
