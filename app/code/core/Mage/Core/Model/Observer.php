1b60c57047baafcdda02b12bd29821f45dcb3cd7
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-05-05 18:34:25 +0100

    coryright

--- app/code/core/Mage/Core/Model/Observer.php
+++ app/code/core/Mage/Core/Model/Observer.php
@@ -6,15 +6,10 @@
  * @package    Mage_Core
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2019-2023 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2024-2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-/**
- * Core Observer model
- *
- * @package    Mage_Core
- */
 class Mage_Core_Model_Observer
 {
     /**
