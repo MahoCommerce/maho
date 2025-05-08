c965a202870f09ead1d78d5283afc1a4382bfc12
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-05-08 22:20:49 +0100

    Solved CodeQL issue 68

--- public/js/mage/adminhtml/tools.js
+++ public/js/mage/adminhtml/tools.js
@@ -767,7 +767,9 @@ function copyText(event) {
  * Clear <div id="messages"></div>
  */
 function clearMessagesDiv(div = null) {
-    setMessagesDivHtml('', div);
+    if (div ??= document.getElementById('messages')) {
+        div.textContent = '';
+    }
 }
 
 /**
@@ -777,17 +779,8 @@ function clearMessagesDiv(div = null) {
  * @param {string} type - one of `success|error|notice`
  */
 function setMessagesDiv(message, type = 'success', div = null) {
-    setMessagesDivHtml(`<ul class="messages"><li class="${type}-msg"><ul><li><span>${message}</span></li></ul></li></ul>`, div);
-}
-
-/**
- * Raw function to update <div id="messages"></div>
- *
- * @param {string} html
-*/
-function setMessagesDivHtml(html, div = null) {
     if (div ??= document.getElementById('messages')) {
-        div.innerHTML = html;
+        div.innerHTML = `<ul class="messages"><li class="${type}-msg"><ul><li><span>${message}</span></li></ul></li></ul>`;
     }
 }
 
