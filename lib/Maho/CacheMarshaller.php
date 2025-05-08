0be9da31b9f5d9e99605056379083f89822a3e0b
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-05-08 20:44:07 +0100

    Added Maho CacheMarshaller

---
+++ lib/Maho/CacheMarshaller.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Maho
+ *
+ * @package    Maho
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Maho;
+
+use Symfony\Component\Cache\Marshaller\MarshallerInterface;
+
+class CacheMarshaller implements MarshallerInterface
+{
+    public function marshall(array $values, ?array &$failed): array
+    {
+        $serialized = $failed = [];
+        foreach ($values as $id => $value) {
+            try {
+                $serialized[$id] = serialize($value);
+            } catch (\Throwable $e) {
+                $failed[] = $id;
+            }
+        }
+
+        return $serialized;
+    }
+
+    public function unmarshall(string $value): mixed
+    {
+        if ('b:0;' === $value) {
+            return false;
+        }
+        if ('N;' === $value) {
+            return null;
+        }
+
+        if (false !== $value = @unserialize($value, ['allowed_classes' => false])) {
+            return $value;
+        }
+    }
+}
