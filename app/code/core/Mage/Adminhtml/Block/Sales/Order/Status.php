9a539146e994525d3a49a5b973f9de1348fc278a
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-05-10 15:02:24 +0100

    whitespace adjustments

--- app/code/core/Mage/Adminhtml/Block/Sales/Order/Status.php
+++ app/code/core/Mage/Adminhtml/Block/Sales/Order/Status.php
@@ -10,7 +10,6 @@
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-
 class Mage_Adminhtml_Block_Sales_Order_Status extends Mage_Adminhtml_Block_Widget_Grid_Container
 {
     /**
--- lib/Varien/Filter/Template.php
+++ lib/Varien/Filter/Template.php
@@ -10,7 +10,6 @@
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-
 class Varien_Filter_Template implements Zend_Filter_Interface
 {
     /**
