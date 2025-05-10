bbcac61a3a37e69d9717008e32daa7b11f577483
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-05-10 11:13:57 +0100

    Added Meta Pixel (Facebook Pixel) support to the GoogleAnalytics module (#166)

--- app/code/core/Mage/GoogleAnalytics/Helper/Data.php
+++ app/code/core/Mage/GoogleAnalytics/Helper/Data.php
@@ -6,15 +6,10 @@
  * @package    Mage_GoogleAnalytics
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2022-2024 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2024-2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-/**
- * GoogleAnalytics data helper
- *
- * @package    Mage_GoogleAnalytics
- */
 class Mage_GoogleAnalytics_Helper_Data extends Mage_Core_Helper_Abstract
 {
     /**
@@ -38,6 +33,16 @@ class Mage_GoogleAnalytics_Helper_Data extends Mage_Core_Helper_Abstract
      * @var string
      */
     protected $_moduleName = 'Mage_GoogleAnalytics';
+
+    public function isMetaPixelEnabled(): bool
+    {
+        return Mage::getStoreConfigFlag('google/meta_pixel/active');
+    }
+
+    public function getMetaPixelId(): string
+    {
+        return Mage::getStoreConfig('google/meta_pixel/account');
+    }
 
     /**
      * Whether GTM is ready to use
