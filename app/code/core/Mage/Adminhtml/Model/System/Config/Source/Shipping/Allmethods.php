635488aab0d1d9ad146c5f8c4661fe2a4a9241c4
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-06-04 23:00:48 +0100

    Fixed shipping methods configuration to handle array method titles (#181)

--- app/code/core/Mage/Adminhtml/Model/System/Config/Source/Shipping/Allmethods.php
+++ app/code/core/Mage/Adminhtml/Model/System/Config/Source/Shipping/Allmethods.php
@@ -6,6 +6,7 @@
  * @package    Mage_Adminhtml
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2022-2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -36,10 +37,20 @@ class Mage_Adminhtml_Model_System_Config_Source_Shipping_Allmethods
                 'value' => [],
             ];
             foreach ($carrierMethods as $methodCode => $methodTitle) {
-                $methods[$carrierCode]['value'][] = [
-                    'value' => $carrierCode . '_' . $methodCode,
-                    'label' => '[' . $carrierCode . '] ' . $methodTitle,
-                ];
+                // Handle cases where methodTitle might be an array (e.g., nested shipping method groups)
+                if (is_array($methodTitle)) {
+                    foreach ($methodTitle as $subMethodCode => $subMethodTitle) {
+                        $methods[$carrierCode]['value'][] = [
+                            'value' => $carrierCode . '_' . $subMethodCode,
+                            'label' => '[' . $carrierCode . '] ' . $subMethodTitle,
+                        ];
+                    }
+                } else {
+                    $methods[$carrierCode]['value'][] = [
+                        'value' => $carrierCode . '_' . $methodCode,
+                        'label' => '[' . $carrierCode . '] ' . $methodTitle,
+                    ];
+                }
             }
         }
 
