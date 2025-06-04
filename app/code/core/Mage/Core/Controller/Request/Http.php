ab543ae1b8806430a9a9edb81a99ae4555ef66e0
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-06-05 00:25:01 +0100

    phpcs

--- app/code/core/Mage/Core/Controller/Request/Http.php
+++ app/code/core/Mage/Core/Controller/Request/Http.php
@@ -6,7 +6,7 @@
  * @package    Mage_Core
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2019-2025 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2024-2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -166,13 +166,13 @@ class Mage_Core_Controller_Request_Http extends Zend_Controller_Request_Http
         // Normalize path info by removing trailing slashes (except for root path)
         if (strlen($pathInfo) > 1 && str_ends_with($pathInfo, '/')) {
             $canonicalPath = rtrim($pathInfo, '/');
-            
+
             // Perform 301 redirect to canonical URL
             $canonicalUrl = $this->getBaseUrl() . $canonicalPath;
             if (!empty($_SERVER['QUERY_STRING'])) {
                 $canonicalUrl .= '?' . $_SERVER['QUERY_STRING'];
             }
-            
+
             Mage::app()->getFrontController()->getResponse()
                 ->setRedirect($canonicalUrl, 301)
                 ->sendResponse();
