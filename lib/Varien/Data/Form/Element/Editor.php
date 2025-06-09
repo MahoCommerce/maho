15835ccc2c3e0eb66d04c95836c3c3dffa290242
Justin Beaty <justin.beaty@gmail.com>
2025-06-09 11:03:44 -0700

    add mahoOnReady

--- lib/Varien/Data/Form/Element/Editor.php
+++ lib/Varien/Data/Form/Element/Editor.php
@@ -50,15 +50,7 @@ class Varien_Data_Form_Element_Editor extends Varien_Data_Form_Element_Textarea
                 {$this->_getButtonsHtml()}
                 <textarea id="{$this->getHtmlId()}" name="{$this->getName()}" {$this->serialize($this->getHtmlAttributes())}>{$this->getEscapedValue()}</textarea>
                 <script>
-                    function onReady(callback) {
-                        if (document.readyState === 'loading') {
-                            document.addEventListener('DOMContentLoaded', callback);
-                        } else {
-                            callback();
-                        }
-                    }
-
-                    onReady(() => {
+                    mahoOnReady(() => {
                         window.$jsSetupObject = new tiptapWysiwygSetup('{$this->getHtmlId()}', $configObject);
                     });
                 </script>
