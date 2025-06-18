e908ea8b235856b565e8dd0c1e8259ed28b9b9bb
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-06-18 19:17:42 +0100

    work

--- app/code/core/Maho/Blog/sql/blog_setup/install-1.0.0.php
+++ app/code/core/Maho/Blog/sql/blog_setup/install-1.0.0.php
@@ -34,9 +34,376 @@ $installer->addEntityType('blog_post', [
     'entity_attribute_collection' => 'eav/entity_attribute_collection',
 ]);
 
-$installer->createEntityTables(
-    $installer->getTable('blog/post'),
-);
+/**
+ * Create table 'blog/post'
+ */
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('blog/post'))
+    ->addColumn('entity_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+        'identity'  => true,
+        'unsigned'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+    ], 'Entity ID')
+    ->addColumn('entity_type_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Entity Type ID')
+    ->addColumn('attribute_set_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Attribute Set ID')
+    ->addColumn('url_key', Varien_Db_Ddl_Table::TYPE_TEXT, 255, [
+        'nullable'  => false,
+    ], 'URL Key')
+    ->addColumn('created_at', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, [
+        'nullable'  => false,
+        'default'   => Varien_Db_Ddl_Table::TIMESTAMP_INIT,
+    ], 'Created At')
+    ->addColumn('updated_at', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, [
+        'nullable'  => false,
+        'default'   => Varien_Db_Ddl_Table::TIMESTAMP_INIT_UPDATE,
+    ], 'Updated At')
+    ->addIndex(
+        $installer->getIdxName('blog/post', ['entity_type_id']),
+        ['entity_type_id'],
+    )
+    ->addIndex(
+        $installer->getIdxName('blog/post', ['url_key']),
+        ['url_key'],
+    )
+    ->addForeignKey(
+        $installer->getFkName('blog/post', 'entity_type_id', 'eav/entity_type', 'entity_type_id'),
+        'entity_type_id',
+        $installer->getTable('eav/entity_type'),
+        'entity_type_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+    )
+    ->setComment('Blog Post Entity Table');
+$installer->getConnection()->createTable($table);
+
+/**
+ * Create table 'blog/post_datetime'
+ */
+$table = $installer->getConnection()
+    ->newTable($installer->getTable(['blog/post', 'datetime']))
+    ->addColumn('value_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+        'identity'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+    ], 'Value ID')
+    ->addColumn('entity_type_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Entity Type ID')
+    ->addColumn('attribute_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Attribute ID')
+    ->addColumn('store_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Store ID')
+    ->addColumn('entity_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Entity ID')
+    ->addColumn('value', Varien_Db_Ddl_Table::TYPE_DATETIME, null, [
+    ], 'Value')
+    ->addIndex(
+        $installer->getIdxName(
+            ['blog/post', 'datetime'],
+            ['entity_id', 'attribute_id', 'store_id'],
+            Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE,
+        ),
+        ['entity_id', 'attribute_id', 'store_id'],
+        ['type' => Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE],
+    )
+    ->addIndex(
+        $installer->getIdxName(['blog/post', 'datetime'], ['attribute_id']),
+        ['attribute_id'],
+    )
+    ->addIndex(
+        $installer->getIdxName(['blog/post', 'datetime'], ['store_id']),
+        ['store_id'],
+    )
+    ->addIndex(
+        $installer->getIdxName(['blog/post', 'datetime'], ['entity_id']),
+        ['entity_id'],
+    )
+    ->addForeignKey(
+        $installer->getFkName(['blog/post', 'datetime'], 'attribute_id', 'eav/attribute', 'attribute_id'),
+        'attribute_id',
+        $installer->getTable('eav/attribute'),
+        'attribute_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+    )
+    ->addForeignKey(
+        $installer->getFkName(['blog/post', 'datetime'], 'entity_id', 'blog/post', 'entity_id'),
+        'entity_id',
+        $installer->getTable('blog/post'),
+        'entity_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+    )
+    ->addForeignKey(
+        $installer->getFkName(['blog/post', 'datetime'], 'store_id', 'core/store', 'store_id'),
+        'store_id',
+        $installer->getTable('core/store'),
+        'store_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+    )
+    ->setComment('Blog Post Datetime Attribute Backend Table');
+$installer->getConnection()->createTable($table);
+
+/**
+ * Create table 'blog/post_int'
+ */
+$table = $installer->getConnection()
+    ->newTable($installer->getTable(['blog/post', 'int']))
+    ->addColumn('value_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+        'identity'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+    ], 'Value ID')
+    ->addColumn('entity_type_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Entity Type ID')
+    ->addColumn('attribute_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Attribute ID')
+    ->addColumn('store_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Store ID')
+    ->addColumn('entity_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Entity ID')
+    ->addColumn('value', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+    ], 'Value')
+    ->addIndex(
+        $installer->getIdxName(
+            ['blog/post', 'int'],
+            ['entity_id', 'attribute_id', 'store_id'],
+            Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE,
+        ),
+        ['entity_id', 'attribute_id', 'store_id'],
+        ['type' => Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE],
+    )
+    ->addIndex(
+        $installer->getIdxName(['blog/post', 'int'], ['attribute_id']),
+        ['attribute_id'],
+    )
+    ->addIndex(
+        $installer->getIdxName(['blog/post', 'int'], ['store_id']),
+        ['store_id'],
+    )
+    ->addIndex(
+        $installer->getIdxName(['blog/post', 'int'], ['entity_id']),
+        ['entity_id'],
+    )
+    ->addForeignKey(
+        $installer->getFkName(['blog/post', 'int'], 'attribute_id', 'eav/attribute', 'attribute_id'),
+        'attribute_id',
+        $installer->getTable('eav/attribute'),
+        'attribute_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+    )
+    ->addForeignKey(
+        $installer->getFkName(['blog/post', 'int'], 'entity_id', 'blog/post', 'entity_id'),
+        'entity_id',
+        $installer->getTable('blog/post'),
+        'entity_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+    )
+    ->addForeignKey(
+        $installer->getFkName(['blog/post', 'int'], 'store_id', 'core/store', 'store_id'),
+        'store_id',
+        $installer->getTable('core/store'),
+        'store_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+    )
+    ->setComment('Blog Post Integer Attribute Backend Table');
+$installer->getConnection()->createTable($table);
+
+/**
+ * Create table 'blog/post_text'
+ */
+$table = $installer->getConnection()
+    ->newTable($installer->getTable(['blog/post', 'text']))
+    ->addColumn('value_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+        'identity'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+    ], 'Value ID')
+    ->addColumn('entity_type_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Entity Type ID')
+    ->addColumn('attribute_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Attribute ID')
+    ->addColumn('store_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Store ID')
+    ->addColumn('entity_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Entity ID')
+    ->addColumn('value', Varien_Db_Ddl_Table::TYPE_TEXT, '64k', [
+    ], 'Value')
+    ->addIndex(
+        $installer->getIdxName(
+            ['blog/post', 'text'],
+            ['entity_id', 'attribute_id', 'store_id'],
+            Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE,
+        ),
+        ['entity_id', 'attribute_id', 'store_id'],
+        ['type' => Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE],
+    )
+    ->addIndex(
+        $installer->getIdxName(['blog/post', 'text'], ['attribute_id']),
+        ['attribute_id'],
+    )
+    ->addIndex(
+        $installer->getIdxName(['blog/post', 'text'], ['store_id']),
+        ['store_id'],
+    )
+    ->addIndex(
+        $installer->getIdxName(['blog/post', 'text'], ['entity_id']),
+        ['entity_id'],
+    )
+    ->addForeignKey(
+        $installer->getFkName(['blog/post', 'text'], 'attribute_id', 'eav/attribute', 'attribute_id'),
+        'attribute_id',
+        $installer->getTable('eav/attribute'),
+        'attribute_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+    )
+    ->addForeignKey(
+        $installer->getFkName(['blog/post', 'text'], 'entity_id', 'blog/post', 'entity_id'),
+        'entity_id',
+        $installer->getTable('blog/post'),
+        'entity_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+    )
+    ->addForeignKey(
+        $installer->getFkName(['blog/post', 'text'], 'store_id', 'core/store', 'store_id'),
+        'store_id',
+        $installer->getTable('core/store'),
+        'store_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+    )
+    ->setComment('Blog Post Text Attribute Backend Table');
+$installer->getConnection()->createTable($table);
+
+/**
+ * Create table 'blog/post_varchar'
+ */
+$table = $installer->getConnection()
+    ->newTable($installer->getTable(['blog/post', 'varchar']))
+    ->addColumn('value_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+        'identity'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+    ], 'Value ID')
+    ->addColumn('entity_type_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Entity Type ID')
+    ->addColumn('attribute_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Attribute ID')
+    ->addColumn('store_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Store ID')
+    ->addColumn('entity_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+        'unsigned'  => true,
+        'nullable'  => false,
+        'default'   => '0',
+    ], 'Entity ID')
+    ->addColumn('value', Varien_Db_Ddl_Table::TYPE_TEXT, 255, [
+    ], 'Value')
+    ->addIndex(
+        $installer->getIdxName(
+            ['blog/post', 'varchar'],
+            ['entity_id', 'attribute_id', 'store_id'],
+            Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE,
+        ),
+        ['entity_id', 'attribute_id', 'store_id'],
+        ['type' => Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE],
+    )
+    ->addIndex(
+        $installer->getIdxName(['blog/post', 'varchar'], ['attribute_id']),
+        ['attribute_id'],
+    )
+    ->addIndex(
+        $installer->getIdxName(['blog/post', 'varchar'], ['store_id']),
+        ['store_id'],
+    )
+    ->addIndex(
+        $installer->getIdxName(['blog/post', 'varchar'], ['entity_id']),
+        ['entity_id'],
+    )
+    ->addForeignKey(
+        $installer->getFkName(['blog/post', 'varchar'], 'attribute_id', 'eav/attribute', 'attribute_id'),
+        'attribute_id',
+        $installer->getTable('eav/attribute'),
+        'attribute_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+    )
+    ->addForeignKey(
+        $installer->getFkName(['blog/post', 'varchar'], 'entity_id', 'blog/post', 'entity_id'),
+        'entity_id',
+        $installer->getTable('blog/post'),
+        'entity_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+    )
+    ->addForeignKey(
+        $installer->getFkName(['blog/post', 'varchar'], 'store_id', 'core/store', 'store_id'),
+        'store_id',
+        $installer->getTable('core/store'),
+        'store_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+        Varien_Db_Ddl_Table::ACTION_CASCADE,
+    )
+    ->setComment('Blog Post Varchar Attribute Backend Table');
+$installer->getConnection()->createTable($table);
 
 // Common attribute properties
 $attributes = [
