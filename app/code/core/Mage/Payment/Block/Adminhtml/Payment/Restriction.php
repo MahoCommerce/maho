8c5d7bdee0ffd52af0b793f0258529639b179e6f
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-06-19 11:34:55 +0100

    Added "payment restrictions" functionality (#182)

---
+++ app/code/core/Mage/Payment/Block/Adminhtml/Payment/Restriction.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Maho
+ *
+ * @package    Mage_Payment
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Mage_Payment_Block_Adminhtml_Payment_Restriction extends Mage_Adminhtml_Block_Widget_Grid_Container
+{
+    public function __construct()
+    {
+        $this->_controller = 'adminhtml_payment_restriction';
+        $this->_blockGroup = 'payment';
+        $this->_headerText = Mage::helper('payment')->__('Payment Restrictions');
+        $this->_addButtonLabel = Mage::helper('payment')->__('Add New Restriction');
+        parent::__construct();
+    }
+}
