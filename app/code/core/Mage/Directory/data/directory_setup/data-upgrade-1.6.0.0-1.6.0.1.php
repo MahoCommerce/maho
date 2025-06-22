23e0c63689d2f9fae661355fe9b0c37989505ae6
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-06-22 10:01:44 +0100

    Converted collection to direct DB access in install script

--- app/code/core/Mage/Directory/data/directory_setup/data-upgrade-1.6.0.0-1.6.0.1.php
+++ app/code/core/Mage/Directory/data/directory_setup/data-upgrade-1.6.0.0-1.6.0.1.php
@@ -6,6 +6,7 @@
  * @package    Mage_Directory
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2020-2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -21,12 +22,10 @@ $installer->getConnection()->insert(
     ],
 );
 
-$countries = [];
-foreach (Mage::helper('directory')->getCountryCollection() as $country) {
-    if ($country->getRegionCollection()->getSize() > 0) {
-        $countries[] = $country->getId();
-    }
-}
+$select = $installer->getConnection()->select()
+    ->distinct()
+    ->from($installer->getTable('directory/country_region'), 'country_id');
+$countries = $installer->getConnection()->fetchCol($select);
 
 $installer->getConnection()->insert(
     $installer->getTable('core/config_data'),
