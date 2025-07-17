725df6a94242b070e08027582bdc762ffc1dfa48
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-07-17 23:43:51 +0100

    copy

--- lib/Varien/Convert/Adapter/Http/Curl.php
+++ lib/Varien/Convert/Adapter/Http/Curl.php
@@ -6,13 +6,12 @@
  * @package    Varien_Convert
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2022-2024 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2024-2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 class Varien_Convert_Adapter_Http_Curl extends Varien_Convert_Adapter_Abstract
 {
-    // load method
     #[\Override]
     public function load()
     {
