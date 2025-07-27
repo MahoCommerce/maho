4d4c2fe03d68824fd3e97545f0ffb9cc8d4a6914
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-07-27 18:02:19 +0100

    work

--- app/code/core/Mage/Catalog/Block/Product/View/Options/Type/Date.php
+++ app/code/core/Mage/Catalog/Block/Product/View/Options/Type/Date.php
@@ -15,13 +15,6 @@
  */
 class Mage_Catalog_Block_Product_View_Options_Type_Date extends Mage_Catalog_Block_Product_View_Options_Abstract
 {
-    /**
-     * Fill date and time options with leading zeros or not
-     *
-     * @var bool
-     */
-    protected $_fillLeadingZeros = true;
-
     #[\Override]
     protected function _prepareLayout()
     {
@@ -115,31 +108,6 @@ class Mage_Catalog_Block_Product_View_Options_Type_Date extends Mage_Catalog_Blo
         return $html;
     }
 
-    /**
-     * Date (dd/mm/yyyy) html drop-downs
-     *
-     * @return string Formatted Html
-     */
-    public function getDropDownsDateHtml()
-    {
-        $fieldsSeparator = '&nbsp;';
-        $fieldsOrder = Mage::getSingleton('catalog/product_option_type_date')->getConfigData('date_fields_order');
-        $fieldsOrder = str_replace(',', $fieldsSeparator, $fieldsOrder);
-
-        $monthsHtml = $this->_getSelectFromToHtml('month', 1, 12);
-        $daysHtml = $this->_getSelectFromToHtml('day', 1, 31);
-
-        $yearStart = Mage::getSingleton('catalog/product_option_type_date')->getYearStart();
-        $yearEnd = Mage::getSingleton('catalog/product_option_type_date')->getYearEnd();
-        $yearsHtml = $this->_getSelectFromToHtml('year', $yearStart, $yearEnd);
-
-        $translations = [
-            'd' => $daysHtml,
-            'm' => $monthsHtml,
-            'y' => $yearsHtml,
-        ];
-        return strtr($fieldsOrder, $translations);
-    }
 
     /**
      * Time input - now uses native time input
@@ -198,75 +166,8 @@ class Mage_Catalog_Block_Product_View_Options_Type_Date extends Mage_Catalog_Blo
         return $html;
     }
 
-    /**
-     * Return drop-down html with range of values
-     *
-     * @param string $name      Id/name of html select element
-     * @param string|int $from  Start position
-     * @param string|int $to    End position
-     * @param string $value     Value selected
-     * @return string           Formatted Html
-     */
-    protected function _getSelectFromToHtml($name, $from, $to, $value = null)
-    {
-        $options = [
-            ['value' => '', 'label' => '-'],
-        ];
-        for ($i = $from; $i <= $to; $i++) {
-            $options[] = ['value' => $i, 'label' => $this->_getValueWithLeadingZeros($i)];
-        }
-        return $this->_getHtmlSelect($name, $value)
-            ->setOptions($options)
-            ->getHtml();
-    }
 
-    /**
-     * HTML select element
-     *
-     * @param string $name Id/name of html select element
-     * @param string|null $value
-     * @return Mage_Core_Block_Html_Select
-     */
-    protected function _getHtmlSelect($name, $value = null)
-    {
-        $option = $this->getOption();
 
-        $require = '';
-        $select = $this->getLayout()->createBlock('core/html_select')
-            ->setId('options_' . $this->getOption()->getId() . '_' . $name)
-            ->setClass('product-custom-option datetime-picker' . $require)
-            ->setExtraParams()
-            ->setName('options[' . $option->getId() . '][' . $name . ']');
-
-        $extraParams = 'style="width:auto"';
-        if (!$this->getSkipJsReloadPrice()) {
-            $extraParams .= ' onchange="opConfig.reloadPrice()"';
-        }
-        $select->setExtraParams($extraParams);
-
-        if (is_null($value)) {
-            $value = $this->getProduct()->getPreconfiguredValues()->getData('options/' . $option->getId() . '/' . $name);
-        }
-        if (!is_null($value)) {
-            $select->setValue($value);
-        }
-
-        return $select;
-    }
-
-    /**
-     * Add Leading Zeros to number less than 10
-     *
-     * @param int $value
-     * @return int|string
-     */
-    protected function _getValueWithLeadingZeros($value)
-    {
-        if (!$this->_fillLeadingZeros) {
-            return $value;
-        }
-        return $value < 10 ? '0' . $value : $value;
-    }
 
     /**
      * DateTime-local input for combined date and time
