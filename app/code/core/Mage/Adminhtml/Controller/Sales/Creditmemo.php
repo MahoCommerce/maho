b61150468bc0cbc226750c36b4f0691073f82d64
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-07-31 00:21:10 +0100

    work

--- app/code/core/Mage/Adminhtml/Controller/Sales/Creditmemo.php
+++ app/code/core/Mage/Adminhtml/Controller/Sales/Creditmemo.php
@@ -95,7 +95,7 @@ class Mage_Adminhtml_Controller_Sales_Creditmemo extends Mage_Adminhtml_Controll
                 ->addAttributeToSelect('*')
                 ->addAttributeToFilter('entity_id', ['in' => $creditmemosIds])
                 ->load();
-            $pdf = Mage::getModel('sales/order_pdf_creditmemo')->getPdf($invoices);
+            $pdf = Mage::getModel('sales/order_pdf_creditmemo')->getPdf($invoices->getItems());
 
             return $this->_prepareDownloadResponse('creditmemo' . Mage::getSingleton('core/date')->date('Y-m-d_H-i-s') .
                 '.pdf', $pdf, 'application/pdf');
