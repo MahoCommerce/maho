51a60f492a1e880593ff0b4f9d7dde85043171f4
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-07-31 15:56:09 +0100

    Replaced `Zend_Pdf` with `DomPdf` (#223)

--- app/code/core/Mage/Bundle/Model/Sales/Order/Pdf/Items/Shipment.php
+++ app/code/core/Mage/Bundle/Model/Sales/Order/Pdf/Items/Shipment.php
@@ -4,155 +4,54 @@
  * Maho
  *
  * @package    Mage_Bundle
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
- * @copyright  Copyright (c) 2020-2024 The OpenMage Contributors (https://openmage.org)
- * @copyright  Copyright (c) 2024 Maho (https://mahocommerce.com)
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 class Mage_Bundle_Model_Sales_Order_Pdf_Items_Shipment extends Mage_Bundle_Model_Sales_Order_Pdf_Items_Abstract
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setTemplate('sales/order/pdf/shipment/items/bundle.phtml');
+    }
+
     /**
-     * Draw item line
-     *
+     * Get bundle children items for display in shipment
+     */
+    public function getBundleChildren(): array
+    {
+        $children = $this->getChilds($this->getItem());
+        if (!$children) {
+            return [];
+        }
+
+        $bundleChildren = [];
+        foreach ($children as $child) {
+            // For shipments, show items that are shipped separately
+            if ($this->isShipmentSeparately($child)) {
+                $bundleChildren[] = $child;
+            }
+        }
+
+        return $bundleChildren;
+    }
+
+    /**
+     * Get bundle options for display
      */
     #[\Override]
-    public function draw()
+    public function getBundleOptions($item = null): array
     {
-        $item   = $this->getItem();
-        $pdf    = $this->getPdf();
-        $page   = $this->getPage();
+        return parent::getBundleOptions($item ?? $this->getItem());
+    }
 
-        $this->_setFontRegular();
-
-        $shipItems = $this->getChilds($item);
-        $orderItems = array_merge([$item->getOrderItem()], $item->getOrderItem()->getChildrenItems());
-
-        $_prevOptionId = '';
-        $drawItems = [];
-
-        foreach ($orderItems as $orderItem) {
-            $line   = [];
-
-            $attributes = $this->getSelectionAttributes($orderItem);
-            if (is_array($attributes)) {
-                $optionId   = $attributes['option_id'];
-            } else {
-                $optionId = 0;
-            }
-
-            if (!isset($drawItems[$optionId])) {
-                $drawItems[$optionId] = [
-                    'lines'  => [],
-                    'height' => 15,
-                ];
-            }
-
-            if ($orderItem->getParentItem()) {
-                if ($_prevOptionId != $attributes['option_id']) {
-                    $line[0] = [
-                        'font'  => 'italic',
-                        'text'  => Mage::helper('core/string')->str_split($attributes['option_label'], 60, true, true),
-                        'feed'  => 60,
-                    ];
-
-                    $drawItems[$optionId] = [
-                        'lines'  => [$line],
-                        'height' => 15,
-                    ];
-
-                    $line = [];
-
-                    $_prevOptionId = $attributes['option_id'];
-                }
-            }
-
-            if (($this->isShipmentSeparately() && $orderItem->getParentItem())
-                || (!$this->isShipmentSeparately() && !$orderItem->getParentItem())
-            ) {
-                if (isset($shipItems[$orderItem->getId()])) {
-                    $qty = $shipItems[$orderItem->getId()]->getQty() * 1;
-                } elseif ($orderItem->getIsVirtual()) {
-                    $qty = Mage::helper('bundle')->__('N/A');
-                } else {
-                    $qty = 0;
-                }
-            } else {
-                $qty = '';
-            }
-
-            $line[] = [
-                'text'  => $qty,
-                'feed'  => 35,
-            ];
-
-            // draw Name
-            if ($orderItem->getParentItem()) {
-                $feed = 65;
-                $name = $this->getValueHtml($orderItem);
-            } else {
-                $feed = 60;
-                $name = $orderItem->getName();
-            }
-            $text = [];
-            foreach (Mage::helper('core/string')->str_split($name, 60, true, true) as $part) {
-                $text[] = $part;
-            }
-            $line[] = [
-                'text'  => $text,
-                'feed'  => $feed,
-            ];
-
-            // draw SKUs
-            $text = [];
-            foreach (Mage::helper('core/string')->str_split($orderItem->getSku(), 25) as $part) {
-                $text[] = $part;
-            }
-            $line[] = [
-                'text'  => $text,
-                'feed'  => 440,
-            ];
-
-            $drawItems[$optionId]['lines'][] = $line;
-        }
-
-        // custom options
-        $options = $item->getOrderItem()->getProductOptions();
-        if ($options) {
-            if (isset($options['options'])) {
-                foreach ($options['options'] as $option) {
-                    $lines = [];
-                    $lines[][] = [
-                        'text'  => Mage::helper('core/string')->str_split(strip_tags($option['label']), 70, true, true),
-                        'font'  => 'italic',
-                        'feed'  => 60,
-                    ];
-
-                    if ($option['value']) {
-                        $text = [];
-                        $printValue = $option['print_value'] ?? strip_tags($option['value']);
-                        $values = explode(', ', $printValue);
-                        foreach ($values as $value) {
-                            foreach (Mage::helper('core/string')->str_split($value, 50, true, true) as $str) {
-                                $text[] = $str;
-                            }
-                        }
-
-                        $lines[][] = [
-                            'text'  => $text,
-                            'feed'  => 65,
-                        ];
-                    }
-
-                    $drawItems[] = [
-                        'lines'  => $lines,
-                        'height' => 15,
-                    ];
-                }
-            }
-        }
-
-        $page = $pdf->drawLineBlocks($page, $drawItems, ['table_header' => true]);
-        $this->setPage($page);
+    /**
+     * Get formatted value HTML for bundle child item
+     */
+    #[\Override]
+    public function getValueHtml(mixed $item): string
+    {
+        return parent::getValueHtml($item);
     }
 }
---
+++ app/code/core/Mage/Sales/Block/Order/Pdf/Creditmemo.php
@@ -0,0 +1,207 @@
+<?php
+
+/**
+ * Maho
+ *
+ * @package    Mage_Sales
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Mage_Sales_Block_Order_Pdf_Creditmemo extends Mage_Sales_Block_Order_Pdf_Abstract
+{
+    protected ?Mage_Sales_Model_Order_Creditmemo $_creditmemo = null;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setTemplate('sales/order/pdf/creditmemo/default.phtml');
+    }
+
+    public function setDocument(Mage_Sales_Model_Order_Creditmemo $creditmemo): self
+    {
+        $this->_creditmemo = $creditmemo;
+        $this->_order = $creditmemo->getOrder();
+        return $this;
+    }
+
+    public function setOrder(Mage_Sales_Model_Order $order): self
+    {
+        $this->_order = $order;
+        return $this;
+    }
+
+    public function getCreditmemo(): ?Mage_Sales_Model_Order_Creditmemo
+    {
+        return $this->_creditmemo;
+    }
+
+    public function getOrder(): ?Mage_Sales_Model_Order
+    {
+        return $this->_order;
+    }
+
+    public function getCreditmemoNumber(): string
+    {
+        return $this->_creditmemo ? $this->_creditmemo->getIncrementId() : '';
+    }
+
+    public function getCreditmemoDate(): string
+    {
+        if ($this->_creditmemo) {
+            return Mage::helper('core')->formatDate($this->_creditmemo->getCreatedAt(), 'medium', false);
+        }
+        return '';
+    }
+
+    public function getOrderNumber(): string
+    {
+        if ($this->_order) {
+            return $this->_order->getRealOrderId();
+        }
+        return '';
+    }
+
+    public function getOrderDate(): string
+    {
+        if ($this->_order) {
+            return Mage::helper('core')->formatDate($this->_order->getCreatedAtStoreDate(), 'medium', false);
+        }
+        return '';
+    }
+
+    public function getBillingAddress(): ?Mage_Sales_Model_Order_Address
+    {
+        return $this->_order ? $this->_order->getBillingAddress() : null;
+    }
+
+    public function getShippingAddress(): ?Mage_Sales_Model_Order_Address
+    {
+        return $this->_order ? $this->_order->getShippingAddress() : null;
+    }
+
+    public function getPaymentInfo(): string
+    {
+        if (!$this->_order || !$this->_order->getPayment()) {
+            return '';
+        }
+
+        $payment = $this->_order->getPayment();
+        $paymentBlock = Mage::helper('payment')->getInfoBlock($payment)
+            ->setIsSecureMode(true);
+
+        // Use HTML output instead of PDF output since we're generating HTML
+        return $paymentBlock->toHtml();
+    }
+
+    public function getItems(): array
+    {
+        $items = [];
+        if ($this->_creditmemo) {
+            foreach ($this->_creditmemo->getAllItems() as $item) {
+                if ($item->getOrderItem()->getParentItem()) {
+                    continue;
+                }
+                $items[] = $item;
+            }
+        }
+        return $items;
+    }
+
+    public function getItemHtml(Mage_Sales_Model_Order_Creditmemo_Item $item): string
+    {
+        $orderItem = $item->getOrderItem();
+        $type = $orderItem->getProductType();
+
+        $renderer = $this->_getItemRenderer($type);
+        if (!$renderer) {
+            $renderer = $this->_getItemRenderer('default');
+        }
+
+        $renderer->setItem($item);
+        $renderer->setOrder($this->getOrder());
+        $renderer->setSource($this->getCreditmemo());
+
+        return $renderer->toHtml();
+    }
+
+    protected function _getItemRenderer(string $type): ?Mage_Sales_Model_Order_Pdf_Items_Abstract
+    {
+        $rendererModel = Mage::getStoreConfig('sales_pdf/creditmemo/' . $type) ?: 'sales/order_pdf_items_creditmemo_default';
+        if (!isset($this->_renderers[$type])) {
+            $this->_renderers[$type] = new Mage_Sales_Model_Order_Pdf_Items_Creditmemo_Default();
+        }
+        return $this->_renderers[$type];
+    }
+
+    protected array $_renderers = [];
+
+    public function getTotals(): array
+    {
+        if (!$this->_creditmemo) {
+            return [];
+        }
+
+        $totals = [];
+
+        // Subtotal
+        $totals[] = [
+            'label' => $this->__('Subtotal'),
+            'value' => $this->formatPrice($this->_creditmemo->getSubtotal()),
+        ];
+
+        // Discount
+        if (abs($this->_creditmemo->getDiscountAmount()) > 0.01) {
+            $totals[] = [
+                'label' => $this->__('Discount'),
+                'value' => $this->formatPrice(-$this->_creditmemo->getDiscountAmount()),
+            ];
+        }
+
+        // Shipping
+        if (!$this->_order->getIsVirtual() && abs($this->_creditmemo->getShippingAmount()) > 0.01) {
+            $totals[] = [
+                'label' => $this->__('Shipping & Handling'),
+                'value' => $this->formatPrice($this->_creditmemo->getShippingAmount()),
+            ];
+        }
+
+        // Adjustment Refund
+        if (abs($this->_creditmemo->getAdjustmentPositive()) > 0.01) {
+            $totals[] = [
+                'label' => $this->__('Adjustment Refund'),
+                'value' => $this->formatPrice($this->_creditmemo->getAdjustmentPositive()),
+            ];
+        }
+
+        // Adjustment Fee
+        if (abs($this->_creditmemo->getAdjustmentNegative()) > 0.01) {
+            $totals[] = [
+                'label' => $this->__('Adjustment Fee'),
+                'value' => $this->formatPrice($this->_creditmemo->getAdjustmentNegative()),
+            ];
+        }
+
+        // Tax
+        if (abs($this->_creditmemo->getTaxAmount()) > 0.01) {
+            $totals[] = [
+                'label' => $this->__('Tax'),
+                'value' => $this->formatPrice($this->_creditmemo->getTaxAmount()),
+            ];
+        }
+
+        // Grand Total
+        $totals[] = [
+            'label' => $this->__('Grand Total'),
+            'value' => $this->formatPrice($this->_creditmemo->getGrandTotal()),
+            'strong' => true,
+        ];
+
+        return $totals;
+    }
+
+    public function formatPrice(float $price): string
+    {
+        return $this->_order->formatPriceTxt($price);
+    }
+}
