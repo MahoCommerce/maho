bfaea2af84458dc234f0f1ca9c0a6c1c613b7a03
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-08-05 23:28:46 +0100

    work

--- app/code/core/Mage/Core/sql/maho_setup/maho-25.08.0.php
+++ app/code/core/Mage/Core/sql/maho_setup/maho-25.08.0.php
@@ -9,7 +9,7 @@
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-/** @var Mage_Core_Model_Resource_Setup $installer */
+/** @var Mage_Core_Model_Resource_Setup $this */
 $installer = $this;
 
 $installer->startSetup();
@@ -49,7 +49,7 @@ foreach ($maxLevelConfigs as $config) {
             'path' => 'dev/log/min_level',
             'value' => $newValue,
         ],
-        ['value']
+        ['value'],
     );
 }
 
