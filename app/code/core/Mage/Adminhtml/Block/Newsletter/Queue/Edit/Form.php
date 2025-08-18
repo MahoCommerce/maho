486fc8bacdf7f5219c3b44d0be1338efc1f87ab1
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-08-18 23:37:05 +0100

    work

--- app/code/core/Mage/Adminhtml/Block/Newsletter/Queue/Edit/Form.php
+++ app/code/core/Mage/Adminhtml/Block/Newsletter/Queue/Edit/Form.php
@@ -55,6 +55,16 @@ class Mage_Adminhtml_Block_Newsletter_Queue_Edit_Form extends Mage_Adminhtml_Blo
                     'value'     => Mage::app()->getStore(true)->getId(),
                 ]);
             }
+
+            if (Mage::helper('core')->isModuleEnabled('Maho_CustomerSegmentation')) {
+                $fieldset->addField('customer_segments', 'multiselect', [
+                    'name'     => 'customer_segments[]',
+                    'label'    => Mage::helper('newsletter')->__('Customer Segments'),
+                    'title'    => Mage::helper('newsletter')->__('Customer Segments'),
+                    'values'   => $this->getCustomerSegmentOptions(),
+                    'note'     => Mage::helper('newsletter')->__('Leave empty to send to all newsletter subscribers. Select specific segments to target only those customers.'),
+                ]);
+            }
         } else {
             $fieldset->addField('date', 'date', [
                 'name'      => 'start_at',
@@ -178,5 +188,26 @@ class Mage_Adminhtml_Block_Newsletter_Queue_Edit_Form extends Mage_Adminhtml_Blo
 
         $this->setForm($form);
         return $this;
+    }
+
+    /**
+     * Get customer segment options for multiselect
+     * Only called if CustomerSegmentation module is enabled
+     */
+    protected function getCustomerSegmentOptions(): array
+    {
+        $collection = Mage::getResourceModel('customersegmentation/segment_collection')
+            ->addFieldToFilter('is_active', 1)
+            ->setOrder('name', 'ASC');
+
+        $options = [];
+        foreach ($collection as $segment) {
+            $options[] = [
+                'value' => $segment->getId(),
+                'label' => $segment->getName(),
+            ];
+        }
+
+        return $options;
     }
 }
