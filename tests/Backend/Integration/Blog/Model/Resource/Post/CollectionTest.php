e46263f83b6f2029ee985655a47d64381376cae4
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-08-24 23:47:50 +0100

    work

--- tests/Backend/Integration/Blog/Model/Resource/Post/CollectionTest.php
+++ tests/Backend/Integration/Blog/Model/Resource/Post/CollectionTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Maho
+ *
+ * @category   Tests
+ * @package    Maho_Blog
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
 declare(strict_types=1);
 
 uses(Tests\MahoBackendTestCase::class);
