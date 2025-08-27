cccdddbc3754a3b22e502b45543f6c026ee633f9
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-08-27 08:41:37 +0100

    work

--- app/code/core/Maho/Blog/Model/Observer.php
+++ app/code/core/Maho/Blog/Model/Observer.php
@@ -116,17 +116,12 @@ class Maho_Blog_Model_Observer
         return $posts;
     }
 
-    /**
-     * Handle search autocomplete content collection event
-     */
     public function addBlogAutocompleteContent(Varien_Event_Observer $observer): void
     {
         $autocompleteData = $observer->getEvent()->getAutocompleteData();
         $layout = $autocompleteData->getLayout();
 
-        // Create blog autocomplete block
         $blogBlock = $layout->createBlock('blog/autocomplete');
-
         if ($blogBlock) {
             $blogHtml = $blogBlock->toHtml();
 
