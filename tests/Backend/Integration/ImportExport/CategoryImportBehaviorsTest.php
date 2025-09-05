0d7e55cbb563e5a7c6735e1aee5923295217c37b
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-09-05 10:09:01 +0100

    work

--- tests/Backend/Integration/ImportExport/CategoryImportBehaviorsTest.php
+++ tests/Backend/Integration/ImportExport/CategoryImportBehaviorsTest.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Maho
+ *
+ * @copyright  Copyright (c) 2025 Maho (https://mahocommerce.com)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
 declare(strict_types=1);
 
 use Tests\MahoBackendTestCase;
