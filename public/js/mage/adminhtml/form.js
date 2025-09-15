75e30c3fe15c4d8d9a1ec592c4c724d75de14f64
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-09-15 20:42:55 +0200

    work

--- public/js/mage/adminhtml/form.js
+++ public/js/mage/adminhtml/form.js
@@ -63,7 +63,6 @@ class varienForm {
             method: 'POST',
             body: formData
         })
-        .then(response => response.json())
         .then(data => {
             this._processValidationResult(data);
         })
