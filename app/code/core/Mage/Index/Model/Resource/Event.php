f828f9a73ca7b779a6fc7a3efbdffbce7b0ac989
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-11-14 17:08:22 +0000

    Fixed all function.impossibleType PHPStan errors (#366)

--- app/code/core/Mage/Index/Model/Resource/Event.php
+++ app/code/core/Mage/Index/Model/Resource/Event.php
@@ -93,7 +93,7 @@ class Mage_Index_Model_Resource_Event extends Mage_Core_Model_Resource_Db_Abstra
             $whereCondition = ['process_id = ?' => $process->getId()];
         } elseif (is_array($process) && !empty($process)) {
             $whereCondition = ['process_id IN (?)' => $process];
-        } elseif (!is_array($whereCondition)) {
+        } else {
             $whereCondition = ['process_id = ?' => $process];
         }
         $this->_getWriteAdapter()->update(
