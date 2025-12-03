323a9f865d255df9f1acb83390fa5de9529ba046
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-12-03 23:18:59 +0000

    work

--- app/code/core/Mage/Customer/Block/Account/Dashboard.php
+++ app/code/core/Mage/Customer/Block/Account/Dashboard.php
@@ -6,14 +6,11 @@
  * @package    Mage_Customer
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2020-2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2025-2026 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
- * Customer dashboard block
- *
- * @package    Mage_Customer
- *
  * @method string getRefererUrl()
  * @method $this setRefererUrl(string $value)
  */
