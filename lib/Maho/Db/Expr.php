e7b5c9c409041ea9412df488013df5d9e1087b6d
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-12-07 21:06:04 +0000

    work

--- lib/Maho/Db/Expr.php
+++ lib/Maho/Db/Expr.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * Maho
  *
- * @package    Maho_Db
+ * @package    Maho_Lib
  * @copyright  Copyright (c) 2025-2026 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
