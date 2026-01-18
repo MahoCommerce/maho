e7075a6a1da81dc5792ce01bcbffeac38dacbad4
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-01-18 22:08:30 +0000

    Fixed license in onestepcheckout.js to AFL

--- public/js/varien/onestepcheckout.js
+++ public/js/varien/onestepcheckout.js
@@ -1,10 +1,8 @@
 /**
  * Maho
  *
- * One-Step Checkout JavaScript
- *
- * @copyright  Copyright (c) 2025-2026 Maho (https://mahocommerce.com)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright  Copyright (c) 2026 Maho (https://mahocommerce.com)
+ * @license    https://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 
 /**
