949bf3fe20cb3c3ff44a7b65b3bb5f18e3c4cb5c
Matthew Campbell <mcampbell@mageaustralia.com.au>
2026-01-22 10:00:25 +1100

    Added GTIN and MPN as default product attributes (#522)
    - GTIN (label: GTIN/UPC/EAN/Barcode) - Global Trade Item Number
    - MPN (label: MPN - Manufacturer Part Number)
    
    Both attributes are varchar, searchable, and assigned to the General group.
    Required for modern ecommerce integrations (Google Shopping, Meta Commerce, etc.)

---
+++ app/code/core/Mage/Catalog/sql/catalog_setup/upgrade-1.6.0.0.19.1.7-1.6.0.0.19.1.8.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Maho
+ *
+ * @package    Mage_Catalog
+ * @copyright  Copyright (c) 2025-2026 Maho (https://mahocommerce.com)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/** @var Mage_Catalog_Model_Resource_Setup $this */
+$installer = $this;
+$installer->startSetup();
+
+// Add GTIN attribute (Global Trade Item Number - encompasses UPC, EAN, ISBN, ITF-14)
+$installer->addAttribute('catalog_product', 'gtin', [
+    'type'                       => 'varchar',
+    'label'                      => 'GTIN/UPC/EAN/Barcode',
+    'input'                      => 'text',
+    'required'                   => false,
+    'sort_order'                 => 5,
+    'global'                     => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'searchable'                 => true,
+    'visible_in_advanced_search' => true,
+    'comparable'                 => false,
+    'used_in_product_listing'    => false,
+    'unique'                     => false,
+    'apply_to'                   => '',
+    'group'                      => 'General',
+]);
+
+// Add MPN attribute (Manufacturer Part Number)
+$installer->addAttribute('catalog_product', 'mpn', [
+    'type'                       => 'varchar',
+    'label'                      => 'MPN (Manufacturer Part Number)',
+    'input'                      => 'text',
+    'required'                   => false,
+    'sort_order'                 => 6,
+    'global'                     => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'searchable'                 => true,
+    'visible_in_advanced_search' => true,
+    'comparable'                 => false,
+    'used_in_product_listing'    => false,
+    'unique'                     => false,
+    'apply_to'                   => '',
+    'group'                      => 'General',
+]);
+
+$installer->endSetup();
