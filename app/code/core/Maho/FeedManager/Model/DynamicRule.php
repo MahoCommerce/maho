MahoCommerce/maho

commit 3e92381a61ef4c1c1628f66fa1390eab45d71a4a

Migrated remaining serialize/unserialize to JSON (#552)

app/code/core/Maho/FeedManager/Model/DynamicRule.php
@@ -112,15 +112,7 @@ class Maho_FeedManager_Model_DynamicRule extends Mage_Core_Model_Abstract
         if ($this->_cases === null) {
             $casesJson = $this->getData('cases');
             if ($casesJson) {
-                try {
-                    $this->_cases = Mage::helper('core')->jsonDecode($casesJson) ?: [];
-                } catch (\JsonException $e) {
-                    Mage::log(
-                        'Failed to decode cases JSON for rule ' . $this->getId() . ': ' . $e->getMessage(),
-                        Mage::LOG_WARNING,
-                    );
-                    $this->_cases = [];
-                }
+                $this->_cases = Mage::helper('core/string')->unserialize($casesJson) ?: [];
             } else {
                 $this->_cases = [];
             }

app/code/core/Mage/Admin/Model/User.php
@@ -141,7 +141,7 @@ class Mage_Admin_Model_User extends Mage_Core_Model_Abstract
             'lastname'  => $this->getLastname(),
             'email'     => $this->getEmail(),
             'modified'  => Mage_Core_Model_Locale::now(),
-            'extra'     => serialize($this->getExtra()),
+            'extra'     => Mage::helper('core')->jsonEncode($this->getExtra()),
         ];
 
         if ($this->getId() > 0) {
@@ -207,7 +207,7 @@ class Mage_Admin_Model_User extends Mage_Core_Model_Abstract
     public function saveExtra($data)
     {
         if (is_array($data)) {
-            $data = serialize($data);
+            $data = Mage::helper('core')->jsonEncode($data);
         }
         $this->_getResource()->saveExtra($this, $data);
         return $this;

app/code/core/Mage/Adminhtml/Model/Sales/Order/Create.php
@@ -435,7 +435,7 @@ class Mage_Adminhtml_Model_Sales_Order_Create extends \Maho\DataObject implement
                     [
                         'product' => $item->getProduct(),
                         'code' => 'additional_options',
-                        'value' => serialize($additionalOptions),
+                        'value' => Mage::helper('core')->jsonEncode($additionalOptions),
                     ],
                 ));
             }
@@ -574,7 +574,7 @@ class Mage_Adminhtml_Model_Sales_Order_Create extends \Maho\DataObject implement
                         $info = $item->getOptionByCode('info_buyRequest');
                         if ($info) {
                             $info = new \Maho\DataObject(
-                                unserialize($info->getValue(), ['allowed_classes' => false]),
+                                Mage::helper('core/string')->unserialize($info->getValue()),
                             );
                             $info->setQty($qty);
                             $info->setOptions($this->_prepareOptionsForRequest($item));
@@ -984,7 +984,7 @@ class Mage_Adminhtml_Model_Sales_Order_Create extends \Maho\DataObject implement
                 [
                     'product' => $item->getProduct(),
                     'code' => 'additional_options',
-                    'value' => serialize($options['additional_options']),
+                    'value' => Mage::helper('core')->jsonEncode($options['additional_options']),
                 ],
             ));
         }
@@ -1498,7 +1498,7 @@ class Mage_Adminhtml_Model_Sales_Order_Create extends \Maho\DataObject implement
             }
             $addOptions = $item->getOptionByCode('additional_options');
             if ($addOptions) {
-                $options['additional_options'] = unserialize($addOptions->getValue(), ['allowed_classes' => false]);
+                $options['additional_options'] = Mage::helper('core/string')->unserialize($addOptions->getValue());
             }
             $item->setProductOrderOptions($options);
         }

app/code/core/Mage/Adminhtml/Model/System/Config/Backend/Serialized.php
@@ -38,7 +38,7 @@ class Mage_Adminhtml_Model_System_Config_Backend_Serialized extends Mage_Core_Mo
     protected function _beforeSave()
     {
         if (is_array($this->getValue())) {
-            $this->setValue(serialize($this->getValue()));
+            $this->setValue(Mage::helper('core')->jsonEncode($this->getValue()));
         }
         return $this;
     }

app/code/core/Mage/Adminhtml/Model/System/Config/Backend/Serialized/Array.php
@@ -20,7 +20,7 @@ class Mage_Adminhtml_Model_System_Config_Backend_Serialized_Array extends Mage_A
     protected function _beforeSave()
     {
         try {
-            Mage::helper('core/unserializeArray')->unserialize(serialize($this->getValue()));
+            Mage::helper('core')->jsonEncode($this->getValue());
         } catch (Exception $e) {
             Mage::throwException(Mage::helper('adminhtml')->__('Serialized data is incorrect'));
         }

app/code/core/Mage/Bundle/Block/Adminhtml/Sales/Order/Items/Renderer.php
@@ -6,6 +6,7 @@
  * @package    Mage_Bundle
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2018-2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2024-2026 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -137,7 +138,7 @@ class Mage_Bundle_Block_Adminhtml_Sales_Order_Items_Renderer extends Mage_Adminh
             $options = $item->getOrderItem()->getProductOptions();
         }
         if (isset($options['bundle_selection_attributes'])) {
-            return unserialize($options['bundle_selection_attributes'], ['allowed_classes' => false]);
+            return Mage::helper('core/string')->unserialize($options['bundle_selection_attributes']);
         }
         return null;
     }

app/code/core/Mage/Bundle/Block/Adminhtml/Sales/Order/View/Items/Renderer.php
@@ -6,6 +6,7 @@
  * @package    Mage_Bundle
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2020-2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2024-2026 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -99,7 +100,7 @@ class Mage_Bundle_Block_Adminhtml_Sales_Order_View_Items_Renderer extends Mage_A
             $options = $item->getOrderItem()->getProductOptions();
         }
         if (isset($options['bundle_selection_attributes'])) {
-            return unserialize($options['bundle_selection_attributes'], ['allowed_classes' => false]);
+            return Mage::helper('core/string')->unserialize($options['bundle_selection_attributes']);
         }
         return null;
     }

app/code/core/Mage/Bundle/Block/Sales/Order/Items/Renderer.php
@@ -6,6 +6,7 @@
  * @package    Mage_Bundle
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
  * @copyright  Copyright (c) 2020-2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2024-2026 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -105,7 +106,7 @@ class Mage_Bundle_Block_Sales_Order_Items_Renderer extends Mage_Sales_Block_Orde
             $options = $item->getOrderItem()->getProductOptions();
         }
         if (isset($options['bundle_selection_attributes'])) {
-            return unserialize($options['bundle_selection_attributes'], ['allowed_classes' => false]);
+            return Mage::helper('core/string')->unserialize($options['bundle_selection_attributes']);
         }
         return null;
     }

commit 5e2a9d5eb9246de2d0a8c03f1657852650f6eb2e

Fixed missing getConditions() on DynamicRule causing fatal error
Regression from 3e92381 which refactored Feed to extend Mage_Rule_Model_Abstract
but left DynamicRule extending Mage_Core_Model_Abstract without a getConditions()
method. This caused a "Call to a member function setJsFormObject() on null" fatal
error when editing or creating dynamic rules.

app/code/core/Maho/FeedManager/Model/DynamicRule.php
@@ -47,6 +47,11 @@ class Maho_FeedManager_Model_DynamicRule extends Mage_Core_Model_Abstract
     protected ?array $_cases = null;
 
     /**
+     * Conditions model instance
+     */
+    protected ?Maho_FeedManager_Model_Rule_Condition_Combine $_conditions = null;
+
+    /**
      * Form instance for conditions rendering
      */
     protected ?\Maho\Data\Form $_form = null;
@@ -69,6 +74,29 @@ class Maho_FeedManager_Model_DynamicRule extends Mage_Core_Model_Abstract
     {
         $this->_form = $form;
         return $this;
+    }
+
+    /**
+     * Get conditions model instance (required by rules engine UI)
+     */
+    public function getConditions(): Maho_FeedManager_Model_Rule_Condition_Combine
+    {
+        if ($this->_conditions === null) {
+            $this->_conditions = Mage::getModel('feedmanager/rule_condition_combine');
+            $this->_conditions->setRule($this)->setId('1')->setPrefix('conditions');
+
+            if ($this->hasConditionsSerialized()) {
+                $conditions = $this->getConditionsSerialized();
+                if (!empty($conditions)) {
+                    $decoded = Mage::helper('core')->jsonDecode($conditions);
+                    if (is_array($decoded) && !empty($decoded)) {
+                        $this->_conditions->loadArray($decoded);
+                    }
+                }
+                $this->unsConditionsSerialized();
+            }
+        }
+        return $this->_conditions;
     }
 
     #[\Override]