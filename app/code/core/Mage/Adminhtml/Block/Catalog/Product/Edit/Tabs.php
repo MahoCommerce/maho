025949d20ca0dcb6e69f764b82d80a0efd147b13
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-02-27 13:48:05 +0000

    Add getTab() to Widget_Tabs and code identifier to product EAV tabs
    Allows retrieving and modifying tabs via events, and distinguishing
    product attribute group tabs by name.
    
    Ported from OpenMage/magento-lts#5219, thanks @Hanmac

--- app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tabs.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tabs.php
@@ -5,7 +5,7 @@
  *
  * @package    Mage_Adminhtml
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
- * @copyright  Copyright (c) 2022-2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2022-2026 The OpenMage Contributors (https://openmage.org)
  * @copyright  Copyright (c) 2024-2026 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
@@ -53,6 +53,7 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Tabs extends Mage_Adminhtml_Bloc
 
                 $this->addTab('group_' . $group->getId(), [
                     'label'     => Mage::helper('catalog')->__($group->getAttributeGroupName()),
+                    'code'      => $group->getAttributeGroupName(),
                     'content'   => $this->_translateHtml($this->getLayout()->createBlock(
                         $this->getAttributeTabBlock(),
                         'adminhtml.catalog.product.edit.tab.attributes',
--- app/code/core/Mage/Adminhtml/Block/Widget/Tabs.php
+++ app/code/core/Mage/Adminhtml/Block/Widget/Tabs.php
@@ -5,7 +5,7 @@
  *
  * @package    Mage_Adminhtml
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
- * @copyright  Copyright (c) 2019-2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2019-2026 The OpenMage Contributors (https://openmage.org)
  * @copyright  Copyright (c) 2024-2026 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
@@ -76,6 +76,11 @@ class Mage_Adminhtml_Block_Widget_Tabs extends Mage_Adminhtml_Block_Widget
     {
         $this->_destElementId = $elementId;
         return $this;
+    }
+
+    public function getTab(string $tabId): null|Mage_Adminhtml_Block_Widget_Tab_Interface|\Maho\DataObject
+    {
+        return $this->_tabs[$tabId] ?? null;
     }
 
     /**
