515c2c62f4d4d7483125a0b88e88789fb6d58eba
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-03-03 17:04:51 +0000

    Use Node.COMMENT_NODE constant instead of magic number

--- public/js/mage/adminhtml/tools.js
+++ public/js/mage/adminhtml/tools.js
@@ -617,5 +617,5 @@ function xssFilter(str) {
     const sanitize = xssKillah();
     const safeNodes = sanitize(str);
     // Filter out comment nodes (nodeType 8) to prevent their text from leaking as visible content
-    return Array.from(safeNodes).filter(n => n.nodeType !== 8).map(n => n.outerHTML || n.textContent).join('');
+    return Array.from(safeNodes).filter(n => n.nodeType !== Node.COMMENT_NODE).map(n => n.outerHTML || n.textContent).join('');
 }
