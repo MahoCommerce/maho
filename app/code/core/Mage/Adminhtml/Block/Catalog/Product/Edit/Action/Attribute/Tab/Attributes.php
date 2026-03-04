b26ec35aad70e6b512da0835d6e5ac1e54df4ee9
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-03-04 14:28:40 +0000

    Fixed bulk attribute update `toggleFieldEditMode` crash (#620)

--- app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Action/Attribute/Tab/Attributes.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Action/Attribute/Tab/Attributes.php
@@ -85,7 +85,7 @@ class Mage_Adminhtml_Block_Catalog_Product_Edit_Action_Attribute_Tab_Attributes 
         $nameAttributeHtml = ($element->getExtType() === 'multiple') ? 'name="' . $element->getId() . '_checkbox"'
             : '';
         return '<span class="attribute-change-checkbox"><input type="checkbox" id="' . $element->getId()
-             . '-checkbox" ' . $nameAttributeHtml . ' onclick="toogleFieldEditMode(this, \'' . $element->getId()
+             . '-checkbox" ' . $nameAttributeHtml . ' onclick="toggleFieldEditMode(this, \'' . $element->getId()
              . '\')" /><label for="' . $element->getId() . '-checkbox">' . Mage::helper('catalog')->__('Change')
              . '</label></span>
                 <script type="text/javascript">initDisableFields(\'' . $element->getId() . '\')</script>';
