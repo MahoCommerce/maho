2a3b21e35878e822e1230f93dd3ff9df2af790c6
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-03-05 09:32:03 +0000

    Added `Maho_ShippingBridge` module for external shipping rate APIs (#623)

---
+++ app/code/core/Maho/ShippingBridge/Helper/Data.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Maho
+ *
+ * @package    Maho_ShippingBridge
+ * @copyright  Copyright (c) 2026 Maho (https://mahocommerce.com)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Maho_ShippingBridge_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    protected $_moduleName = 'Maho_ShippingBridge';
+}
