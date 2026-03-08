48b13be3130472522216af194b1a2cb7be0cd7d0
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-03-08 15:29:51 +0000

    work

--- app/code/core/Mage/Customer/Helper/Data.php
+++ app/code/core/Mage/Customer/Helper/Data.php
@@ -109,7 +109,7 @@ class Mage_Customer_Helper_Data extends Mage_Core_Helper_Abstract
      * @var array<string, string>
      */
     protected array $_vatPatterns = [
-        'AT' => '/^ATU\d{8}$/',
+        'AT' => '/^ATU[A-Z\d]{8}$/',
         'BE' => '/^BE[01]\d{9}$/',
         'BG' => '/^BG\d{9,10}$/',
         'CH' => '/^CHE\d{9}(MWST|TVA|IVA)?$/',
@@ -119,27 +119,27 @@ class Mage_Customer_Helper_Data extends Mage_Core_Helper_Abstract
         'DK' => '/^DK\d{8}$/',
         'EE' => '/^EE\d{9}$/',
         'EL' => '/^EL\d{9}$/',
-        'ES' => '/^ES([A-Z]\d{7}[A-Z]|\d{8}[A-Z])$/',
+        'ES' => '/^ES([A-Z]\d{7}[A-Z]|\d{8}[A-Z]|[A-Z]\d{8})$/',
         'FI' => '/^FI\d{8}$/',
-        'FR' => '/^FR[A-HJ-NP-Z0-9]{2}\d{9}$/',
-        'GB' => '/^GB(\d{9}|\d{12}|GD\d{3}|HA\d{3})$/',
+        'FR' => '/^FR[A-Z0-9]{2}\d{9}$/',
+        'GB' => '/^GB(\d{9,12}|(GD|HA)\d{3})$/',
         'HR' => '/^HR\d{11}$/',
         'HU' => '/^HU\d{8}$/',
-        'IE' => '/^IE[0-9A-Z+*]{7}[A-Z]{1,2}$/',
+        'IE' => '/^IE([A-Z\d]{8}|[A-Z\d]{9})$/',
         'IT' => '/^IT\d{11}$/',
         'LT' => '/^LT(\d{9}|\d{12})$/',
         'LU' => '/^LU\d{8}$/',
         'LV' => '/^LV\d{11}$/',
         'MT' => '/^MT\d{8}$/',
-        'NL' => '/^NL[A-Z0-9+*]{9}B\d{2}$/',
-        'NO' => '/^NO\d{9}MVA$/',
+        'NL' => '/^NL\d{9}B\d{2}$/',
+        'NO' => '/^NO\d{9}(MVA)?$/',
         'PL' => '/^PL\d{10}$/',
         'PT' => '/^PT\d{9}$/',
         'RO' => '/^RO\d{2,10}$/',
         'SE' => '/^SE\d{12}$/',
         'SI' => '/^SI\d{8}$/',
         'SK' => '/^SK\d{10}$/',
-        'XI' => '/^XI(\d{9}|\d{12}|GD\d{3}|HA\d{3})$/',
+        'XI' => '/^XI(\d{9,12}|(GD|HA)\d{3})$/',
     ];
 
     /**
