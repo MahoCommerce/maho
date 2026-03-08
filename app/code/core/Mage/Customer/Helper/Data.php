c1865225f56413e1f1e267a69d8b60402212e139
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-03-08 15:22:55 +0000

    work

--- app/code/core/Mage/Customer/Helper/Data.php
+++ app/code/core/Mage/Customer/Helper/Data.php
@@ -102,21 +102,27 @@ class Mage_Customer_Helper_Data extends Mage_Core_Helper_Abstract
 
     /**
      * VAT format regex patterns by country
+     *
+     * @see https://en.wikipedia.org/wiki/VAT_identification_number
+     * @see https://taxation-customs.ec.europa.eu/taxation/vat/vat-directive/vat-identification-numbers_en
+     *
      * @var array<string, string>
      */
     protected array $_vatPatterns = [
         'AT' => '/^ATU\d{8}$/',
         'BE' => '/^BE[01]\d{9}$/',
         'BG' => '/^BG\d{9,10}$/',
-        'CY' => '/^CY[0-59]\d{7}[A-Z]$/',
+        'CH' => '/^CHE\d{9}(MWST|TVA|IVA)?$/',
+        'CY' => '/^CY\d{8}[A-Z]$/',
         'CZ' => '/^CZ\d{8,10}$/',
-        'DE' => '/^DE[1-9]\d{8}$/',
+        'DE' => '/^DE\d{9}$/',
         'DK' => '/^DK\d{8}$/',
-        'EE' => '/^EE10\d{7}$/',
+        'EE' => '/^EE\d{9}$/',
         'EL' => '/^EL\d{9}$/',
-        'ES' => '/^ES[A-Z0-9]\d{7}[A-Z0-9]$/',
+        'ES' => '/^ES([A-Z]\d{7}[A-Z]|\d{8}[A-Z]|[A-Z]\d{8})$/',
         'FI' => '/^FI\d{8}$/',
         'FR' => '/^FR[A-HJ-NP-Z0-9]{2}\d{9}$/',
+        'GB' => '/^GB(\d{9}|\d{12}|GD\d{3}|HA\d{3})$/',
         'HR' => '/^HR\d{11}$/',
         'HU' => '/^HU\d{8}$/',
         'IE' => '/^IE[0-9A-Z+*]{7}[A-Z]{1,2}$/',
@@ -124,17 +130,16 @@ class Mage_Customer_Helper_Data extends Mage_Core_Helper_Abstract
         'LT' => '/^LT(\d{9}|\d{12})$/',
         'LU' => '/^LU\d{8}$/',
         'LV' => '/^LV\d{11}$/',
-        'MT' => '/^MT[1-9]\d{7}$/',
-        'NL' => '/^NL\d{9}B\d{2}$/',
+        'MT' => '/^MT\d{8}$/',
+        'NL' => '/^NL[A-Z0-9+*]{9}B\d{2}$/',
+        'NO' => '/^NO\d{9}MVA$/',
         'PL' => '/^PL\d{10}$/',
         'PT' => '/^PT\d{9}$/',
-        'RO' => '/^RO[1-9]\d{1,9}$/',
-        'SE' => '/^SE\d{10}01$/',
-        'SI' => '/^SI[1-9]\d{7}$/',
-        'SK' => '/^SK[1-9]\d{9}$/',
-        'GB' => '/^GB(\d{9}|\d{12}|GD\d{3}|HA\d{3})$/',
-        'CH' => '/^CHE\d{9}(MWST|TVA|IVA)?$/',
-        'NO' => '/^NO\d{9}MVA$/',
+        'RO' => '/^RO\d{2,10}$/',
+        'SE' => '/^SE\d{12}$/',
+        'SI' => '/^SI\d{8}$/',
+        'SK' => '/^SK\d{10}$/',
+        'XI' => '/^XI(\d{9}|\d{12}|GD\d{3}|HA\d{3})$/',
     ];
 
     /**
@@ -144,7 +149,7 @@ class Mage_Customer_Helper_Data extends Mage_Core_Helper_Abstract
     protected array $_viesCountries = [
         'AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES',
         'FI', 'FR', 'HR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'MT',
-        'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK',
+        'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK', 'XI',
     ];
 
     /**
