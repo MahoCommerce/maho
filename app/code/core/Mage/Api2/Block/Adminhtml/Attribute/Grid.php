7c2ab5e0a7d59e3a2c05f3814fb2812cb49c0b0e
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-03-22 21:12:26 +0000

    Improved performance for grids with permission checks (#710)

--- app/code/core/Mage/Api2/Block/Adminhtml/Attribute/Grid.php
+++ app/code/core/Mage/Api2/Block/Adminhtml/Attribute/Grid.php
@@ -5,7 +5,7 @@
  *
  * @package    Mage_Api2
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://magento.com)
- * @copyright  Copyright (c) 2019-2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2019-2026 The OpenMage Contributors (https://openmage.org)
  * @copyright  Copyright (c) 2024-2026 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
@@ -77,9 +77,7 @@ class Mage_Api2_Block_Adminhtml_Attribute_Grid extends Mage_Adminhtml_Block_Widg
     #[\Override]
     public function getRowUrl($row)
     {
-        /** @var Mage_Admin_Model_Session $session */
-        $session = Mage::getSingleton('admin/session');
-        if ($session->isAllowed('system/api/attributes/edit')) {
+        if ($this->isAllowed('system/api/attributes/edit')) {
             return $this->getUrl('*/*/edit', ['type' => $row->getUserTypeCode()]);
         }
 
