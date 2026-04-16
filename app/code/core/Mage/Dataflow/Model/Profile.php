9c27ab344648d81b96b3c54c3b608570aed1449d
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-04-16 19:33:33 +0100

    Apply rector: replace strlen() > 0 with !== '' comparison

--- app/code/core/Mage/Dataflow/Model/Profile.php
+++ app/code/core/Mage/Dataflow/Model/Profile.php
@@ -85,7 +85,7 @@ class Mage_Dataflow_Model_Profile extends Mage_Core_Model_Abstract
     {
         parent::_beforeSave();
         $actionsXML = $this->getData('actions_xml');
-        if ($actionsXML !== null && strlen($actionsXML) > 0 &&
+        if ($actionsXML !== null && (string) $actionsXML !== '' &&
             @simplexml_load_string('<data>' . $actionsXML . '</data>', null, LIBXML_NOERROR) === false
         ) {
             Mage::throwException(Mage::helper('dataflow')->__('Actions XML is not valid.'));
