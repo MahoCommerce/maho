d6b0557775f17142c98f57d6c12540a62387c9a8
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-04-17 23:17:40 +0100

    Composer update

--- app/code/core/Mage/Api/Model/Server/Handler/Abstract.php
+++ app/code/core/Mage/Api/Model/Server/Handler/Abstract.php
@@ -125,7 +125,7 @@ abstract class Mage_Api_Model_Server_Handler_Abstract
         }
         $this->_getServer()->getAdapter()->fault(
             $faults[$faultName]['code'],
-            (is_null($customMessage) ? $faults[$faultName]['message'] : $customMessage),
+            ($customMessage ?? $faults[$faultName]['message']),
         );
     }
 
@@ -150,7 +150,7 @@ abstract class Mage_Api_Model_Server_Handler_Abstract
         return [
             'isFault'      => true,
             'faultCode'    => $faults[$faultName]['code'],
-            'faultMessage' => (is_null($customMessage) ? $faults[$faultName]['message'] : $customMessage),
+            'faultMessage' => ($customMessage ?? $faults[$faultName]['message']),
         ];
     }
 
