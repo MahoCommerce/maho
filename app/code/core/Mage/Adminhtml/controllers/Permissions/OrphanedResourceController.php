01027ae66ea7b1a681f7cfa9c06150519635265a
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-04-20 17:26:18 +0100

    Add Maho copyright to controllers, drop routing benchmark script
    bench_routing.php was a one-off local benchmark, no need to ship it.

--- app/code/core/Mage/Adminhtml/controllers/Permissions/OrphanedResourceController.php
+++ app/code/core/Mage/Adminhtml/controllers/Permissions/OrphanedResourceController.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
  *
  * @package    Mage_Adminhtml
  * @copyright  Copyright (c) 2024 The OpenMage Contributors (https://openmage.org)
+ * @copyright  Copyright (c) 2026 Maho (https://mahocommerce.com)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
