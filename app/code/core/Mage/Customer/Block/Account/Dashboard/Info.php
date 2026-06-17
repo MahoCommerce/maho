28933817fa13415a9a8d04d776b27e6201bff93f
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-06-17 17:02:42 +0100

    lint

--- app/code/core/Mage/Customer/Block/Account/Dashboard/Info.php
+++ app/code/core/Mage/Customer/Block/Account/Dashboard/Info.php
@@ -1,6 +1,7 @@
 <?php
 
 /**
+ * SPDX-FileCopyrightText: 2026 Maho <https://mahocommerce.com>
  * SPDX-FileCopyrightText: 2020-2024 The OpenMage Contributors <https://openmage.org>
  * SPDX-FileCopyrightText: 2006-2020 Magento, Inc. <https://magento.com>
  * SPDX-License-Identifier: OSL-3.0
