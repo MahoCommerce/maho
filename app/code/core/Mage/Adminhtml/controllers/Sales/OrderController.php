842ecc5f682de65f69a4fc63ddfee4bfe4c45567
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-06-20 10:19:30 +0100

    Removed the inline translation system (#1044)

--- app/code/core/Mage/Adminhtml/controllers/Sales/OrderController.php
+++ app/code/core/Mage/Adminhtml/controllers/Sales/OrderController.php
@@ -329,11 +329,6 @@ class Mage_Adminhtml_Sales_OrderController extends Mage_Adminhtml_Controller_Act
     {
         $this->_initOrder();
         $html = $this->getLayout()->createBlock('adminhtml/sales_order_view_tab_history')->toHtml();
-        /** @var Mage_Core_Model_Translate_Inline $translate */
-        $translate = Mage::getModel('core/translate_inline');
-        if ($translate->isAllowed()) {
-            $translate->processResponseBody($html);
-        }
         $this->getResponse()->setBody($html);
     }
 
