ea439c4d9d0ad8882d44ab9c93832f529c3f9966
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-09-18 15:17:21 +0100

    Kept accessibility:install as an alias of sys:playwright:install

--- lib/MahoCLI/Commands/SysPlaywrightInstall.php
+++ lib/MahoCLI/Commands/SysPlaywrightInstall.php
@@ -18,6 +18,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
     name: 'sys:playwright:install',
+    aliases: ['accessibility:install'],
     description: 'Install the shared Playwright runtime for the browser-based scanners',
 )]
 class SysPlaywrightInstall extends BaseMahoCommand
